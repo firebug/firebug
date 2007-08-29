97f3c0d50213d2231da0b596281a9de9e9907da5
Max Stepanov <max.v.stepanov@gmail.com>
2007-08-29 11:58:37 +0000

    made a copy
    http://code.google.com/p/fbug/source/detail?r=100

---
+++ defaults/preferences/firebug.js
@@ -0,0 +1,55 @@
+
+// Global
+pref("extensions.firebug.disabledAlways", true);
+pref("extensions.firebug.disabledFile", true);
+pref("extensions.firebug.defaultPanelName", "html");
+pref("extensions.firebug.throttleMessages", true);
+pref("extensions.firebug.textSize", 0);
+pref("extensions.firebug.showInfoTips", true);
+pref("extensions.firebug.largeCommandLine", false);
+pref("extensions.firebug.textWrapWidth", 100);
+pref("extensions.firebug.openInWindow", false);
+pref("extensions.firebug.showErrorCount", true);
+
+// Console
+pref("extensions.firebug.showJSErrors", true);
+pref("extensions.firebug.showJSWarnings", false);
+pref("extensions.firebug.showCSSErrors", false);
+pref("extensions.firebug.showXMLErrors", false);
+pref("extensions.firebug.showWebErrors", true);
+pref("extensions.firebug.showChromeErrors", false);
+pref("extensions.firebug.showChromeMessages", false);
+pref("extensions.firebug.showExternalErrors", false);
+pref("extensions.firebug.showXMLHttpRequests", true);
+pref("extensions.firebug.showStackTrace", true);
+
+// HTML
+pref("extensions.firebug.showCommentNodes", false);
+pref("extensions.firebug.showWhitespaceNodes", false);
+pref("extensions.firebug.showFullTextNodes", true);
+pref("extensions.firebug.highlightMutations", true);
+pref("extensions.firebug.expandMutations", false);
+pref("extensions.firebug.scrollToMutations", false);
+pref("extensions.firebug.shadeBoxModel", true);
+
+// CSS
+pref("extensions.firebug.showComputedStyle", false);
+
+// Script
+pref("extensions.firebug.breakOnErrors", false);
+
+// DOM
+pref("extensions.firebug.showUserProps", true);
+pref("extensions.firebug.showUserFuncs", true);
+pref("extensions.firebug.showDOMProps", true);
+pref("extensions.firebug.showDOMFuncs", false);
+pref("extensions.firebug.showDOMConstants", false);
+
+// Layout
+pref("extensions.firebug.showAdjacentLayout", false);
+pref("extensions.firebug.showRulers", true);
+
+// Net
+pref("extensions.firebug.netFilterCategory", "all");
+pref("extensions.firebug.disableNetMonitor", false);
+pref("extensions.firebug.collectHttpHeaders", true);
