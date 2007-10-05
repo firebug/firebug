8203fda938fd4c57f725c6e99c384996e91ea94a
John J. Barton <johnjbarton@johnjbarton.com>
2007-10-05 21:45:15 +0000

    [1.1 fix] if format is empty string then ignore it.
    http://code.google.com/p/fbug/source/detail?r=202

--- extension/content/firebug/console.js
+++ extension/content/firebug/console.js
@@ -201,18 +201,18 @@ Firebug.ConsolePanel.prototype = extend(Firebug.Panel,
         }
 
         var parts = parseFormat(format);
-		for (var i= 0; i < parts.length; i++)
-		{
-			var part = parts[i];
+        for (var i= 0; i < parts.length; i++)
+        {
+            var part = parts[i];
             if (part && typeof(part) == "object")
             {
-				if (++objIndex > objects.length)  // then too few parameters for format, assume unformatted.
-				{
-					format = "";
-            		objIndex = -1;
-					parts = parseFormat(format);
-					break;
-				}
+                if (++objIndex > objects.length)  // then too few parameters for format, assume unformatted.
+                {
+                    format = "";
+                    objIndex = -1;
+                    parts.length = 0;
+                    break;
+                }
             }
 
 		}
@@ -413,6 +413,8 @@ Firebug.ConsolePanel.prototype = extend(Firebug.Panel,
 function parseFormat(format)
 {
     var parts = [];
+    if (format.length <= 0)
+        return parts;
 
     var reg = /((^%|.%)(\d+)?(\.)([a-zA-Z]))|((^%|.%)([a-zA-Z]))/;
     for (var m = reg.exec(format); m; m = reg.exec(format))
