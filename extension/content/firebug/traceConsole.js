6f8f6f8aaf09267bc97a92d9a3c646b61a803f44
Jan Odvarko <odvarko@gmail.com>
2008-09-01 13:33:23 +0000

    FBTrace console window
    http://code.google.com/p/fbug/source/detail?r=985

---
+++ extension/content/firebug/traceConsole.js
@@ -0,0 +1,85 @@
+/* See license.txt for terms of usage */
+
+// Constants
+//-----------------------------------------------------------------------------
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+
+var gFindBar;
+
+// Implementation
+//-----------------------------------------------------------------------------
+
+var Console =
+{
+    modules: [],
+
+    onLoad: function()
+    {
+        var args = window.arguments[0];
+        this.activeModule = args.traceModule;
+    
+        var consoleFrame = document.getElementById("consoleFrame");
+        this.consoleNode = consoleFrame.contentDocument.getElementById("panelNode-traceConsole");
+
+        // Associate Console with the module object.
+        this.activeModule.onLoadConsole(window, this.consoleNode);
+        this.registerModule(this.activeModule);
+
+        // Find toolbar
+        gFindBar = document.getElementById("FindToolbar");
+    },
+
+    onUnload: function()
+    {
+        for (var i=0; i<this.modules.length; ++i)
+            this.modules[i].onUnloadConsole();
+    },
+    
+    registerModule: function(module)
+    {
+        module.consoleRoot = this.activeModule.consoleRoot;
+        this.modules.push(module);
+    },
+
+    unregisterModule: function(module)
+    {
+        for (var i=0; i<this.modules.length; ++i) {
+            if (this.modules[i] == item) {
+                this.modules.splice(i, 1);
+                break;
+            }
+        }
+    },
+
+    // Commands
+    onClearConsole: function()
+    {
+        var tbody = this.consoleNode.firstChild.firstChild;
+        while (tbody.firstChild)
+            tbody.removeChild(tbody.lastChild);
+    },
+    
+    onSeparateConsole: function()
+    {
+        this.activeModule.dumpSeparator();
+    },
+    
+    onSaveToFile: function()
+    {
+    },
+
+    onRestartFirefox: function()
+    {
+        Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup).
+            quit(Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit);
+    },
+    
+    onExitFirefox: function()
+    {
+        goQuitApplication();
+    }
+}
+
+//-----------------------------------------------------------------------------
