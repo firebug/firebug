5d9699706d8a225af52192182bec1ff1f020ad01
John J. Barton <johnjbarton@johnjbarton.com>
2008-12-16 06:40:23 +0000

    Issue 1281:  	 Watch tab: New Watch expression fails if Firebug is not stopped on a breakpoint
    http://code.google.com/p/fbug/source/detail?r=1537

--- extension/content/firebug/dom.js
+++ extension/content/firebug/dom.js
@@ -374,7 +374,7 @@ Firebug.DOMBasePanel.prototype = extend(Firebug.AblePanel,
     {
         if (hasClass(row, "watchNewRow"))
         {
-            if (Firebug.Console.isEnabled(this.context))
+            if (Firebug.Console.isEnabled(this.context) && Firebug.CommandLine.isReadyElsePreparing(this.context))
                 Firebug.Editor.startEditing(row, "");
         }
         else if (hasClass(row, "watchRow"))
