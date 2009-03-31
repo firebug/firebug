0f4586d400ede759a2365b09c2f5b13de2678949
John J. Barton <johnjbarton@johnjbarton.com>
2009-03-31 00:50:24 +0000

    better tracing on stop stepping
    http://code.google.com/p/fbug/source/detail?r=2340

--- extension/components/firebug-service.js
+++ extension/components/firebug-service.js
@@ -2127,7 +2127,8 @@ FirebugService.prototype =
 
     stopStepping: function()
     {
-        if (FBTrace.DBG_FBS_STEP) FBTrace.sysout("stopStepping\n");
+        if (FBTrace.DBG_FBS_STEP) FBTrace.sysout("stopStepping stepMode = "+getStepName(stepMode)
+                 +" hookFrameCount="+hookFrameCount+" stepFrameCount="+stepFrameCount+"\n");
         stepMode = 0;
         stepFrame = null;
         stepFrameCount = 0;
