2b07b85e1be8a37c2ca7b7bbf17545dc60004b29
Jan Odvarko <odvarko@gmail.com>
2009-04-15 14:07:41 +0000

    Avoid exception in ShortcutsModel + format source so, it's in line with the other Firebug modules.
    http://code.google.com/p/fbug/source/detail?r=2525

--- extension/content/firebug/shortcuts.js
+++ extension/content/firebug/shortcuts.js
@@ -1,55 +1,70 @@
 /* See license.txt for terms of usage */
-FBL.ns( function()
+
+FBL.ns( function() { with (FBL) {
+
+// ************************************************************************************************
+// Constants
+
+const prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch2);
+
+/**
+ * ShortcutsModel implementation
+ * description TODO
+ */
+Firebug.ShortcutsModel = extend(Firebug.Module,
 {
-    with (FBL)
+    dispatchName: "shortcuts",
+
+    initializeUI: function()
     {
-        Firebug.ShortcutsModel = extend(Firebug.Module, {
+        this.initShortcuts();
+    },
 
-            dispatchName: "shortcuts",
+    initShortcuts : function()
+    {
+        var branch = prefs.getBranch("extensions.firebug.key.shortcut.");
+        var shortcutNames = branch.getChildList("", {});
+        shortcutNames.forEach(this.initShortcut);
+    },
 
-            initializeUI: function()
-            {
-                this.initShortcuts();
-            },
+    initShortcut : function(element, index, array)
+    {
+        var branch = prefs.getBranch("extensions.firebug.key.");
+        var shortcut = branch.getCharPref("shortcut." + element);
+        var tokens = shortcut.split(' ');
+        var key = tokens.pop();
+        var modifiers = tokens.join(',')
+        var keyElem = document.getElementById("key_" + element);
+        if (!keyElem)
+        {
+            //if key is not defined in xul, add it
+            keyElem = document.createElement('key');
+            keyElem.className = "fbOnlyKey";
+            keyElem.id = "key_" + element;
+            keyElem.command = "cmd_" + element;
+            $('mainKeyset').appendChild(keyElem);
+        }
 
-            initShortcuts : function() {
-                var branch = prefs.getBranch("extensions.firebug.key.shortcut.");
-                var shortcutNames = branch.getChildList("", {});
-                shortcutNames.forEach(this.initShortcut);
-            },
-
-            initShortcut : function(element, index, array) {
-                var branch = prefs.getBranch("extensions.firebug.key.");
-                var shortcut = branch.getCharPref("shortcut." + element);
-                var tokens = shortcut.split(' ');
-                var key = tokens.pop();
-                var modifiers = tokens.join(',')
-                var keyElem = document.getElementById("key_" + element);
-                if (!keyElem)
-                {
-                    //if key is not defined in xul, add it
-                    keyElem = document.createElement('key');
-                    keyElem.className = "fbOnlyKey";
-                    keyElem.id = "key_" + element;
-                    keyElem.command = "cmd_" + element;
-                    $('mainKeyset').appendChild(keyElem);
-                }
-
-                //choose between key or keycode attribute
-                if (key.length == 1)
-                {
-                    keyElem.setAttribute('modifiers', modifiers);
-                    keyElem.setAttribute('key', key);
-                    keyElem.removeAttribute('keycode');
-                }
-                else if (KeyEvent['DOM_' + key]) //only set valid keycodes
-                {
-                    keyElem.setAttribute('modifiers', modifiers);
-                    keyElem.setAttribute('keycode', key);
-                    keyElem.removeAttribute('key'); //in case default shortcut uses key rather than keycode
-                }
-            }
-        });
-        Firebug.registerModule(Firebug.ShortcutsModel);
+        //choose between key or keycode attribute
+        if (key.length == 1)
+        {
+            keyElem.setAttribute('modifiers', modifiers);
+            keyElem.setAttribute('key', key);
+            keyElem.removeAttribute('keycode');
+        }
+        else if (KeyEvent['DOM_' + key]) //only set valid keycodes
+        {
+            keyElem.setAttribute('modifiers', modifiers);
+            keyElem.setAttribute('keycode', key);
+            keyElem.removeAttribute('key'); //in case default shortcut uses key rather than keycode
+        }
     }
 });
+
+// ************************************************************************************************
+// Registration
+
+Firebug.registerModule(Firebug.ShortcutsModel);
+
+// ************************************************************************************************
+}});
