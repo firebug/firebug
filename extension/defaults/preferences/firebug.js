ebdac4a2b35e4911660c457d7a6787cd5c08b2ca
Hans Hillen <hans.hillen@gmail.com>
2009-05-19 13:54:21 +0000

    Fix for Issue 260: Firebug disables Mac Standard Cmd-Tilde switching
    http://code.google.com/p/fbug/source/detail?r=2916

--- extension/defaults/preferences/firebug.js
+++ extension/defaults/preferences/firebug.js
@@ -148,7 +148,7 @@ pref("extensions.firebug.key.shortcut.toggleFirebug", "VK_F12");
 pref("extensions.firebug.key.shortcut.detachFirebug", "accel VK_F12");
 pref("extensions.firebug.key.shortcut.leftFirebugTab", "accel shift VK_PAGE_UP");
 pref("extensions.firebug.key.shortcut.rightFirebugTab", "accel shift VK_PAGE_DOWN");
-pref("extensions.firebug.key.shortcut.previousFirebugTab", "accel `");
+pref("extensions.firebug.key.shortcut.previousFirebugTab", "control `");
 pref("extensions.firebug.key.shortcut.customizeFBKeys", "alt accel shift k");
 pref("extensions.firebug.key.shortcut.clearConsole", "accel shift r");
 pref("extensions.firebug.key.shortcut.openTraceConsole", "shift r");                             /*@explore*/
