a48d8772dff4ac8aae785e890ece4e2fc152dc18
Jan Odvarko <odvarko@gmail.com>
2009-06-02 16:44:57 +0000

    Remove context from global context array if it shouldn't be used.
    http://code.google.com/p/fbug/source/detail?r=3076

--- extension/content/firebug/tabWatcher.js
+++ extension/content/firebug/tabWatcher.js
@@ -118,10 +118,14 @@ top.TabWatcher = extend(new Firebug.Listener(),
             if (!this.shouldShowContext(context))
             {
                 this.watchContext(win, null);
+
+                // There shouldn't be context for this widnow so, remove it from the 
+                // global array.
+                remove(contexts, context);
+
                 return;  // did not create a context
             }
             // else we should show
-
         }
         else // then we've not looked this window in this session
         {
