1120969412c36401f7787e7d89b88924af4957c7
John J. Barton <johnjbarton@johnjbarton.com>
2009-07-22 00:22:25 +0000

    [&1.4] avoid null for XPCOM error
    http://code.google.com/p/fbug/source/detail?r=3658

--- extension/content/firebug/errors.js
+++ extension/content/firebug/errors.js
@@ -286,6 +286,9 @@ var Errors = Firebug.Errors = extend(Firebug.Module,
     getErrorContext: function(object)
     {
         var url = object.sourceName;
+        if(!url)
+            return null;  // eg some XPCOM messages
+
         var errorContext = this.contextCache[url];
 
         if (errorContext)
