53307d3f2b4ae85aca6e3ac61dacf175e0601e16
John J. Barton <johnjbarton@johnjbarton.com>
2009-07-28 23:42:36 +0000

    Issue 2108:  Firebug leaks at least one chrome window
    http://code.google.com/p/fbug/source/detail?r=3764

--- extension/content/firebug/trace.js
+++ extension/content/firebug/trace.js
@@ -5,18 +5,24 @@
 var FBTrace = Components.classes["@joehewitt.com/firebug-trace-service;1"]
                  .getService(Components.interfaces.nsISupports).wrappedJSObject.getTracer("extensions.firebug");
 FBTrace.setScope(window);
+function clearFBTraceScope()
+{
+    window.removeEventListener('unload', clearFBTraceScope, true);
+    FBTrace.setScope(null);
+}
+window.addEventListener('unload', clearFBTraceScope, true);
 // ************************************************************************************************
 // Some examples of tracing APIs
 
 // 1) Log "Hello World!" into the console.
-//    FBTrace.sysout("Hello World!")       
+//    FBTrace.sysout("Hello World!")
 //
 // 2) Log "Hello World!" if the DBG_ERRORS option is true.
 //    if (FBTrace.DBG_ERRORS)
-//       FBTrace.sysout("Hello World!");  
+//       FBTrace.sysout("Hello World!");
 //
 // 3) Log "Hello World!" and various info about 'world' object.
-//    FBTrace.sysout("Hello World!", world);  
+//    FBTrace.sysout("Hello World!", world);
 //
 // 4) Log into specific console (created by Firebug extension).
 //    FBTrace.dump("firebug.extensions", "Hello World!", world);
