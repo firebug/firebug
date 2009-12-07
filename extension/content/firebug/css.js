707669ed8533d5bb2efdc707e68d00b8b99d8019
John J. Barton <johnjbarton@johnjbarton.com>
2009-12-07 21:26:29 +0000

    extra tracing for the else branch on CSS rules
    http://code.google.com/p/fbug/source/detail?r=5178

--- extension/content/firebug/css.js
+++ extension/content/firebug/css.js
@@ -377,6 +377,9 @@ Firebug.CSSStyleSheetPanel.prototype = extend(Firebug.SourceBoxPanel,
                     rules.push({tag: CSSImportRuleTag.tag, rule: rule});
                 else if (rule instanceof CSSMediaRule)
                     appendRules.apply(this, [rule.cssRules]);
+                else
+                    if (DBG_ERRORS || DBG_CSS)
+                        FBTrace.sysout("css getStyleSheetRules failed to classify a rule ", rule);
             }
         }
 
