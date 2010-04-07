5362a285543a66a0fbeba599e21d870f3b885580
Steven Roussey <sroussey@gmail.com>
2010-04-07 21:41:00 +0000

    Show doctype in red if the given doctype is xhtml and the document itself is not.
    Tests:
    No doctype - http://www.network54.com/
    HTML doctype - http://www.latimes.com/
    HTML5 style doctype - http://www.appcenter.com/
    Correct XHTML - http://www.w3.org/TR/XHTMLplusMathMLplusSVG/sample.xhtml
    Bad XHTML - https://developer.mozilla.org/En/DOM/DocumentType
    Bad XHTML - http://blog.getfirebug.com/
    Bad XHTML - http://www.w3schools.com/
    Bad XHTML - http://www.ibm.com/us/en/
    Bad XHTML - http://www.microsoft.com/en/us/default.aspx
    Bad XHTML - http://techcrunch.com/
    Bad XHTML - everybody???
    
    http://code.google.com/p/fbug/source/detail?r=6496

--- extension/content/firebug/html.js
+++ extension/content/firebug/html.js
@@ -1412,7 +1412,7 @@ Firebug.HTMLPanel.HTMLHtmlElement = domplate(FirebugReps.Element,
 {
     tag:
         DIV({"class": "nodeBox htmlNodeBox containerNodeBox $object|getHidden repIgnore", _repObject: "$object", role :"presentation"},
-            DIV({"class": "docType"},
+            DIV({"class": "docType $object|getErrorClass","title":"$object|getErrorMessage"},
                 "$object|getDocType"
             ),
             DIV({"class": "nodeLabel", role: "presentation"},
@@ -1437,6 +1437,21 @@ Firebug.HTMLPanel.HTMLHtmlElement = domplate(FirebugReps.Element,
     {
         var doctype = obj.ownerDocument.doctype;
         return '<!DOCTYPE ' + doctype.name + (doctype.publicId ? ' PUBLIC "' + doctype.publicId + '"': '') + (doctype.systemId ? ' "' + doctype.systemId + '"' : '') + '>';
+    },
+    getErrorClass: function(obj)
+    {
+        var docstring = this.getDocType(obj),
+            claimXhtml = docstring.indexOf('XHTML')!=-1 || docstring.indexOf('xhtml')!=-1,
+            isXhtml = isElementXHTML(obj);
+        if (claimXhtml && !isXhtml)
+            return "docTypeError";
+        return "";
+    },
+    getErrorMessage: function(obj)
+    {
+        if (this.getErrorClass(obj)!="")
+            return $STR("html.error.doctype.mismatched");
+        return "";
     }
 });
 
