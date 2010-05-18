bd90ef02a5c3a03196a058715a7a1ce14cad1e21
John J. Barton <johnjbarton@johnjbarton.com>
2010-05-18 16:16:20 +0000

    fix console.log([1, 2, eval]) to print [1,2,<exception>]
    http://code.google.com/p/fbug/source/detail?r=6758

--- extension/content/firebug/reps.js
+++ extension/content/firebug/reps.js
@@ -477,12 +477,22 @@ this.Arr = domplate(Firebug.Rep,
         var items = [];
         for (var i = 0; i < array.length && i <= max; ++i)
         {
-            var value = array[i];
-            var rep = Firebug.getRep(value);
-            var tag = rep.shortTag || rep.tag;
-            var delim = (i == array.length-1 ? "" : ", ");
+        	try
+        	{
+        		var delim = (i == array.length-1 ? "" : ", ");
+        		var value = array[i];
+                var rep = Firebug.getRep(value);
+                var tag = rep.shortTag || rep.tag;
 
-            items.push({object: value, tag: tag, delim: delim});
+                items.push({object: value, tag: tag, delim: delim});
+        	}
+        	catch(exc)
+        	{
+        		var rep = Firebug.getRep(exc);
+                var tag = rep.shortTag || rep.tag;
+
+        		items.push({object: exc, tag: tag, delim: delim});
+        	}
         }
 
         if (array.length > max + 1)
