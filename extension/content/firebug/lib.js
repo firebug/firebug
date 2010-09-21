188803c5c89fb827022b1620a4320791b5d92859
Jan Odvarko <odvarko@gmail.com>
2010-09-21 14:43:56 +0000

    [&1.6] Issue 2426: Copy XPath should include namespace
    http://code.google.com/p/fbug/source/detail?r=7843

--- extension/content/firebug/lib.js
+++ extension/content/firebug/lib.js
@@ -1310,16 +1310,21 @@ this.getElementTreeXPath = function(element)
 {
     var paths = [];
 
+    // Use nodeName (instead of localName) so namespace prefix is included (if any).
     for (; element && element.nodeType == 1; element = element.parentNode)
     {
         var index = 0;
         for (var sibling = element.previousSibling; sibling; sibling = sibling.previousSibling)
         {
-            if (sibling.localName == element.localName)
+            // Ignore document type declaration.
+            if (sibling.nodeType == Node.DOCUMENT_TYPE_NODE)
+                continue;
+
+            if (sibling.nodeName == element.nodeName)
                 ++index;
         }
 
-        var tagName = element.localName.toLowerCase();
+        var tagName = element.nodeName.toLowerCase();
         var pathIndex = (index ? "[" + (index+1) + "]" : "");
         paths.splice(0, 0, tagName + pathIndex);
     }
