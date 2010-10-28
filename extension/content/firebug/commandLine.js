39379a0142d1aaa4c23ce62c508bbe295000de8a
John J. Barton <johnjbarton@johnjbarton.com>
2010-10-28 18:03:12 +0000

    [&1.6-dual-track] Issue 3581:  	 Commandline issues... completing stuff I don't want it to
    http://code.google.com/p/fbug/source/detail?r=8195

--- extension/content/firebug/commandLine.js
+++ extension/content/firebug/commandLine.js
@@ -796,7 +796,7 @@ Firebug.CommandLine = extend(Firebug.Module,
             return;
         }
 
-        this.autoCompleter.complete(Firebug.currentContext, commandLine, completionBox, true, false, true);
+        this.autoCompleter.complete(Firebug.currentContext, commandLine, completionBox, true, false);
         Firebug.currentContext.commandLineText = this.autoCompleter.getVerifiedText(commandLine);
     },
 
