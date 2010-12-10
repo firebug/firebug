eeb52bc1bdfe83c819ad55e3b0a92aa10d185523
Jan Odvarko <odvarko@gmail.com>
2010-12-10 15:30:21 +0000

    Put tracing console preferences into separate file so, it's easier to remove for final releases.
    http://code.google.com/p/fbug/source/detail?r=8716

--- extension/defaults/preferences/firebug.js
+++ extension/defaults/preferences/firebug.js
@@ -111,68 +111,6 @@ pref("extensions.firebug.cache.responseLimit", 5242880); // maximum size limit f
 // External Editors
 pref("extensions.firebug.externalEditors", "");
 
-// Trace                                                        /*@explore*/
-pref("extensions.firebug.alwaysOpenTraceConsole", false);       /*@explore*/
-pref("extensions.firebug.trace.maxMessageLength", 400);         /*@explore*/
-pref("extensions.firebug.trace.enableScope", false);            /*@explore*/
-pref("extensions.firebug.trace.showTime", false);               /*@explore*/
-pref("extensions.firebug.DBG_FBS_JSDCONTEXT", false);// firebug trace scriptinfo(huge)   /*@explore*/
-pref("extensions.firebug.DBG_FBS_FF_START", false); // firebug trace from FF start(huge) /*@explore*/
-pref("extensions.firebug.DBG_FBS_CREATION", false); // firebug script creation           /*@explore*/
-pref("extensions.firebug.DBG_FBS_BP", false);       // firebug breakpoints               /*@explore*/
-pref("extensions.firebug.DBG_FBS_SRCUNITS", false); // firebug script creation           /*@explore*/
-pref("extensions.firebug.DBG_FBS_ERRORS", false);   // firebug errors                    /*@explore*/
-pref("extensions.firebug.DBG_FBS_FINDDEBUGGER", false);   // firebug findDebugger        /*@explore*/
-pref("extensions.firebug.DBG_FBS_STEP", false);     // firebug stepping                  /*@explore*/
-pref("extensions.firebug.DBG_FBS_TRACKFILES", false); // dump all js files to disk       /*@explore*/
-pref("extensions.firebug.DBG_FBS_FUNCTION", false); // firebug new Function              /*@explore*/
-pref("extensions.firebug.DBG_ACTIVATION", false);   // firebug.js and tabWatcher.js      /*@explore*/
-pref("extensions.firebug.DBG_BP", false);           // debugger.js and firebug-services.js; lots of output   /*@explore*/
-pref("extensions.firebug.DBG_COMPILATION_UNITS", false);           // debugger.js and firebug-services.js; lots of output   /*@explore*/
-pref("extensions.firebug.DBG_TOPLEVEL", false);     // top level jsd scripts                     /*@explore*/
-pref("extensions.firebug.DBG_STACK", false);        // call stack, mostly debugger.js            /*@explore*/
-pref("extensions.firebug.DBG_UI_LOOP", false);      // debugger.js                               /*@explore*/
-pref("extensions.firebug.DBG_ERRORS", false);       // error.js                                  /*@explore*/
-pref("extensions.firebug.DBG_ERRORLOG", false);       // error.js                                  /*@explore*/
-pref("extensions.firebug.DBG_FUNCTION_NAMES", false);  // heuristics for anon functions          /*@explore*/
-pref("extensions.firebug.DBG_EVAL", false);         // debugger.js and firebug-service.js        /*@explore*/
-pref("extensions.firebug.DBG_PANELS", false);       // panel selection                           /*@explore*/
-pref("extensions.firebug.DBG_CACHE", false);        // sourceCache                               /*@explore*/
-pref("extensions.firebug.DBG_CONSOLE", false);      // console                                   /*@explore*/
-pref("extensions.firebug.DBG_COMMANDLINE", false);  // command line                              /*@explore*/
-pref("extensions.firebug.DBG_CSS", false);          //                                           /*@explore*/
-pref("extensions.firebug.DBG_CSS_PARSER", false);          //                                    /*@explore*/
-pref("extensions.firebug.DBG_DBG2FIREBUG", false);  //                                           /*@explore*/
-pref("extensions.firebug.DBG_DOM", false);          //                                           /*@explore*/
-pref("extensions.firebug.DBG_DOMPLATE", false);     // domplate engine                           /*@explore*/
-pref("extensions.firebug.DBG_DISPATCH", false);     //                                           /*@explore*/
-pref("extensions.firebug.DBG_HTML", false);         //                                           /*@explore*/
-pref("extensions.firebug.DBG_LINETABLE", false);    //                                           /*@explore*/
-pref("extensions.firebug.DBG_LOCATIONS", false);    // panelFileList                             /*@explore*/
-pref("extensions.firebug.DBG_SOURCEFILES", false);  // debugger and sourceCache                  /*@explore*/
-pref("extensions.firebug.DBG_WINDOWS", false);      // tabWatcher, dispatch events; very useful for understand modules/panels  /*@explore*/
-pref("extensions.firebug.DBG_NET", false);          // net.js                                    /*@explore*/
-pref("extensions.firebug.DBG_NET_EVENTS", false);   // net.js - network events                   /*@explore*/
-pref("extensions.firebug.DBG_INITIALIZE", false);   // registry (modules panels); initialize FB  /*@explore*/
-pref("extensions.firebug.DBG_INSPECT", false);      // inspector                                 /*@explore*/
-pref("extensions.firebug.DBG_OPTIONS", false);      //                                           /*@explore*/
-pref("extensions.firebug.DBG_FBS_FLUSH", false);    //                                           /*@explore*/
-pref("extensions.firebug.DBG_HTTPOBSERVER", false); // Centralized HTTP Observer                 /*@explore*/
-pref("extensions.firebug.DBG_SPY", false);          // spy.js                                    /*@explore*/
-pref("extensions.firebug.DBG_JSONVIEWER", false);   // json explorer                             /*@explore*/
-pref("extensions.firebug.DBG_EDITOR", false);       // Inline editors                            /*@explore*/
-pref("extensions.firebug.DBG_SHORTCUTS", false);    // Keyboard shortcuts.                       /*@explore*/
-pref("extensions.firebug.DBG_A11Y", false);         // a11y                                      /*@explore*/
-pref("extensions.firebug.DBG_LOCALE", false);       // localization, missing strings             /*@explore*/
-pref("extensions.firebug.DBG_INFOTIP", false);      // popup info tip in panels                  /*@explore*/
-pref("extensions.firebug.DBG_ANNOTATIONS", false);  // Page annotations service                  /*@explore*/
-pref("extensions.firebug.DBG_XMLVIEWER", false);    // xml explorer                              /*@explore*/
-pref("extensions.firebug.DBG_SVGVIEWER", false);    // svg explorer                              /*@explore*/
-pref("extensions.firebug.DBG_ACTIVITYOBSERVER", false);    // Net panel's activity observer      /*@explore*/
-pref("extensions.firebug.DBG_TOOLTIP", false);      // tooltip debugging      /*@explore*/
-pref("extensions.firebug.DBG_HISTORY", false);      // panel navigation history                  /*@explore*/
-pref("extensions.firebug.DBG_STORAGE", false);      // storageService                  /*@explore*/
-
 // Keyboard
 pref("extensions.firebug.key.shortcut.reenterCommand", "control shift e");
 pref("extensions.firebug.key.shortcut.toggleInspecting", "accel shift c");
@@ -190,9 +128,8 @@ pref("extensions.firebug.key.shortcut.leftFirebugTab", "accel shift VK_PAGE_UP")
 pref("extensions.firebug.key.shortcut.rightFirebugTab", "accel shift VK_PAGE_DOWN");
 pref("extensions.firebug.key.shortcut.previousFirebugTab", "control `");
 pref("extensions.firebug.key.shortcut.clearConsole", "accel shift r");
-pref("extensions.firebug.key.shortcut.openTraceConsole", "shift r");                             /*@explore*/
-pref("extensions.firebug.key.shortcut.navBack", "accel shift VK_LEFT");                          /*@explore*/
-pref("extensions.firebug.key.shortcut.navForward", "accel shift VK_RIGHT");                      /*@explore*/
+pref("extensions.firebug.key.shortcut.navBack", "accel shift VK_LEFT");
+pref("extensions.firebug.key.shortcut.navForward", "accel shift VK_RIGHT");
 pref("extensions.firebug.key.shortcut.increaseTextSize", "accel shift +");
 pref("extensions.firebug.key.shortcut.decreaseTextSize", "accel shift -");
 pref("extensions.firebug.key.shortcut.normalTextSize", "accel shift VK_INSERT");
