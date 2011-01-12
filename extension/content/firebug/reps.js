c63c4988cb433107a3163ccc6d4fcd878b17303e
Jan Odvarko <odvarko@gmail.com>
2011-01-12 16:55:51 +0000

    Fix iteration of dom storage (session, local) for Firefox 4 (the dom/storage test passes again).
    http://code.google.com/p/fbug/source/detail?r=8914

--- extension/content/firebug/reps.js
+++ extension/content/firebug/reps.js
@@ -2038,7 +2038,62 @@ this.Storage = domplate(Firebug.Rep,
 
     propIterator: function(object, max)
     {
-        return FirebugReps.Obj.propIterator(object, max);
+        // we can't utilize the existing function due to:
+        // https://bugzilla.mozilla.org/show_bug.cgi?id=573875
+        //return FirebugReps.Obj.propIterator(object, max);
+
+        max = max || 3;
+        if (!object)
+            return [];
+
+        var props = [];
+        var len = 0, count = 0;
+
+        try
+        {
+            for (var i=0; i<object.length; i++)
+            {
+                var value;
+                var name;
+                try
+                {
+                    name = object.key(i);
+                    value = object.getItem(name);
+                }
+                catch (exc)
+                {
+                    continue;
+                }
+
+                var rep = Firebug.getRep(value);
+                var tag = rep.shortTag || rep.tag;
+
+                count++;
+                if (count <= max)
+                    props.push({tag: tag, name: name, object: value, equal: "=", delim: ", "});
+                else
+                    break;
+            }
+
+            if (count > max)
+            {
+                props[Math.max(1,max-1)] = {
+                    object: "more...", //xxxHonza localization
+                    tag: FirebugReps.Caption.tag,
+                    name: "",
+                    equal:"",
+                    delim:""
+                };
+            }
+            else if (props.length > 0)
+            {
+                props[props.length-1].delim = '';
+            }
+        }
+        catch (exc)
+        {
+        }
+        return props;
     },
 });
 
