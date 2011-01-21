77d69da8b77ad277600ef8ae8a8a8cb051c32452
John J. Barton <johnjbarton@johnjbarton.com>
2011-01-21 22:47:36 +0000

    Remove getRootWindow from INITIALIZE tracing, too much output
    http://code.google.com/p/fbug/source/detail?r=9020

--- extension/content/firebug/tabWatcher.js
+++ extension/content/firebug/tabWatcher.js
@@ -527,7 +527,7 @@ top.TabWatcher = extend(new Firebug.Listener(),
 
         var rootWindow = getRootWindow(winIn);
 
-        if (FBTrace.DBG_INITIALIZE)
+        if (FBTrace.DBG_ROOT_WINDOW) // too much output to use INITIALIZE
             FBTrace.sysout("winIn: "+safeGetWindowLocation(winIn).substr(0,50)+" rootWindow: "+safeGetWindowLocation(rootWindow));
 
         if (rootWindow)
