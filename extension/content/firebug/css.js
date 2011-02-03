c0aad5aaa169598f923f464f42fbf277e30e27be
John J. Barton <johnjbarton@johnjbarton.com>
2011-02-03 21:35:45 +0000

    add explicit scope window
    http://code.google.com/p/fbug/source/detail?r=9194

--- extension/content/firebug/css.js
+++ extension/content/firebug/css.js
@@ -1594,7 +1594,7 @@ CSSElementPanel.prototype = extend(Firebug.CSSStyleSheetPanel.prototype,
 
     updateSelection: function(element)
     {
-        if ( !(element instanceof Element) ) // html supports SourceLink
+        if ( !(element instanceof window.Element) ) // html supports SourceLink
             return;
 
         if (sothinkInstalled)
