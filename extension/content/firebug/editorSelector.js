a61f698002bf8a48ccb881789c7b110eaef99f50
Jan Odvarko <odvarko@gmail.com>
2011-03-17 14:17:15 +0000

    [1.8] Licence note, proper indent, source code formatting fixed.
    http://code.google.com/p/fbug/source/detail?r=9696

--- extension/content/firebug/editorSelector.js
+++ extension/content/firebug/editorSelector.js
@@ -1,94 +1,106 @@
+/* See license.txt for terms of usage */
+
 define("editorSelector.js", ["ToolsInterface"], function(ToolsInterface) { with(FBL) {
-    //************************************************************************************************
-    // Reusable code for modules that support editing
-    Firebug.EditorSelector =
+
+// ********************************************************************************************* //
+// Reusable code for modules that support editing
+
+Firebug.EditorSelector =
+{
+    // Override for each module
+    getEditorOptionKey: function()
     {
-            // Override for each module
-            getEditorOptionKey: function()
+        return "cssEditMode";
+    },
+
+    editors: {},
+
+    registerEditor: function(name, editor)
+    {
+        this.editors[name] = editor;
+    },
+
+    unregisterEditor: function(name, editor)
+    {
+        delete this.editors[name];
+    },
+
+    getEditorByName: function(name)
+    {
+        return this.editors[name];
+    },
+
+    getEditorsNames: function()
+    {
+        var names = [];
+        for (var p in this.editors)
+        {
+            if (this.editors.hasOwnProperty(p))
+                names.push(p);
+        }
+        return names;
+    },
+
+    setCurrentEditorName: function(name)
+    {
+        this.currentEditorName = name;
+        Firebug.Options.set(this.getEditorOptionKey(), name);
+    },
+
+    getCurrentEditorName: function()
+    {
+        if (!this.currentEditorName)
+            this.currentEditorName = Firebug.Options.get(this.getEditorOptionKey());
+
+        return this.currentEditorName;
+    },
+
+    getCurrentEditor: function()
+    {
+        return this.getEditorByName(this.getCurrentEditorName());
+    },
+
+    onEditMode: function(event, menuitem)
+    {
+        var mode = menuitem.getAttribute("mode");
+        if (mode)
+            this.setCurrentEditorName(mode);
+
+        this.updateEditButton();
+        FBL.cancelEvent(event);
+    },
+
+    updateEditButton: function()
+    {
+        // Update label and tooltip text of the edit button.
+        var mode = this.getCurrentEditorName();
+        var menuitem = Firebug.chrome.$("menu_" + this.getEditorOptionKey() + mode);
+
+        var command = Firebug.chrome.$("cmd_toggle"+this.getEditorOptionKey());
+        command.setAttribute("label", menuitem.label);
+        command.setAttribute("tooltiptext", menuitem.tooltipText);
+    },
+
+    onOptionsShowing: function(popup)
+    {
+        var mode = this.getCurrentEditorName();
+
+        for (var child = popup.firstChild; child; child = child.nextSibling)
+        {
+            if (child.localName == "menuitem")
             {
-                return "cssEditMode";
-            },
+                if (child.id == "menu_"+this.getEditorOptionKey()+mode)
+                    child.setAttribute("checked", true);
+                else
+                    child.removeAttribute("checked");
+            }
+        }
+    },
+};
 
-            editors: {},
+// ********************************************************************************************* //
 
-            registerEditor: function(name, editor)
-            {
-                this.editors[name] = editor;
-            },
-            unregisterEditor: function(name, editor)
-            {
-                delete this.editors[name];
-            },
-            getEditorByName: function(name)
-            {
-                return this.editors[name];
-            },
-            getEditorsNames: function()
-            {
-                var names = [];
-                for (var p in this.editors)
-                {
-                    if (this.editors.hasOwnProperty(p))
-                        names.push(p);
-                }
-                return names;
-            },
+return Firebug.EditorSelector;
 
-            setCurrentEditorName: function(name)
-            {
-                this.currentEditorName = name;
-                Firebug.Options.set(this.getEditorOptionKey(), name);
-            },
-            getCurrentEditorName: function()
-            {
-                if (!this.currentEditorName)
-                    this.currentEditorName = Firebug.Options.get(this.getEditorOptionKey());
-
-                return this.currentEditorName;
-            },
-            getCurrentEditor: function()
-            {
-                return this.getEditorByName(this.getCurrentEditorName());
-            },
-
-            onEditMode: function(event, menuitem)
-            {
-                var mode = menuitem.getAttribute("mode");
-                if (mode)
-                    this.setCurrentEditorName(mode);
-
-                this.updateEditButton();
-                FBL.cancelEvent(event);
-            },
-
-            updateEditButton: function()
-            {
-                // Update lable of the edit button according to the preferences.
-                var mode = this.getCurrentEditorName();
-                var menuitem = Firebug.chrome.$("menu_" + this.getEditorOptionKey() + mode);
-
-                var command = Firebug.chrome.$("cmd_toggle"+this.getEditorOptionKey());
-                command.setAttribute("label", menuitem.label);
-                command.setAttribute("tooltiptext", menuitem.tooltipText);
-            },
-
-            onOptionsShowing: function(popup)
-            {
-                var mode = this.getCurrentEditorName();
-
-                for (var child = popup.firstChild; child; child = child.nextSibling)
-                {
-                    if (child.localName == "menuitem")
-                    {
-                        if (child.id == "menu_"+this.getEditorOptionKey()+mode)
-                            child.setAttribute("checked", true);
-                        else
-                            child.removeAttribute("checked");
-                    }
-                }
-            },
-
-    };
-
-    return Firebug.EditorSelector;
+// ********************************************************************************************* //
 }});
