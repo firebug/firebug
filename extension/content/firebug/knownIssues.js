c9e4ae7027c85202c64ecdeb48dab0e47a77cd3d
Jan Odvarko <odvarko@gmail.com>
2011-03-28 12:01:57 +0000

    [1.8 &1.7] Issue 4287: "Issues with trunk Firefox for Firebug"
    http://code.google.com/p/fbug/source/detail?r=9834

--- extension/content/firebug/knownIssues.js
+++ extension/content/firebug/knownIssues.js
@@ -2,7 +2,7 @@
 
 FBL.ns(function() { with (FBL) {
 
-// ************************************************************************************************
+// ********************************************************************************************* //
 // Constants
 
 const Cc = Components.classes;
@@ -11,11 +11,10 @@ const Ci = Components.interfaces;
 const versionChecker = Cc["@mozilla.org/xpcom/version-comparator;1"].getService(Ci.nsIVersionComparator);
 const appInfo = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULAppInfo);
 
-// ************************************************************************************************
+// ********************************************************************************************* //
 
 /**
- * This module is responsible for displaying a link to known issues:
- * http://getfirebug.com/knownissues
+ * This module is responsible for varisous hacky solutions related to known issues.
  */
 Firebug.KnownIssues = extend(Firebug.Module,
 /** @lends Firebug.KnownIssues */
@@ -50,63 +49,16 @@ Firebug.KnownIssues = extend(Firebug.Module,
         if (FBTrace.DBG_INITIALIZE)
             FBTrace.sysout("Set commandLineShowCompleterPopup " +
                 Firebug.commandLineShowCompleterPopup);
-    },
-
-    showPanel: function(browser, panel)
-    {
-        if (!panel)
-            return;
-
-        // Only display if the console panel is actually visible.
-        if (panel && panel.name != "console")
-            return;
-
-        // Don't display if it was already displayed before.
-        if (panel.panelNode.querySelector(".objectLink.knownIssues"))
-            return;
-
-        // Display the message only for Firefox 40
-        if (versionChecker.compare(appInfo.version, "4.0*") < 0)
-            return;
-
-        try
-        {
-            // Log the message into the console.
-            Firebug.Console.log([], panel.context, "info", Firebug.KnownIssuesRep);
-        }
-        catch (e)
-        {
-            if (FBTrace.DBG_ERRORS)
-                FBTrace.sysout("Firebug.KnownIssues.showPanel; EXCEPTION", e);
-        }
     }
 });
 
-// ************************************************************************************************
-
-Firebug.KnownIssuesRep = domplate(Firebug.Rep,
-{
-    className: "text",
-
-    tag:
-        FirebugReps.OBJECTBOX({onclick: ""},
-            SPAN($STR("message.knownIssues40")),
-            SPAN("&nbsp;"),
-            SPAN({"class": "objectLink knownIssues", style: "color:blue", onclick: "$onclick"},
-                "http://getfirebug.com/knownissues"
-            )
-        ),
-
-    onclick: function(event)
-    {
-        openNewTab("http://getfirebug.com/knownissues");
-    }
-});
-
-// ************************************************************************************************
+// ********************************************************************************************* //
 
 Firebug.registerModule(Firebug.KnownIssues);
 
-// ************************************************************************************************
+// ********************************************************************************************* //
+
 return Firebug.KnownIssues;
+
+// ********************************************************************************************* //
 }});
