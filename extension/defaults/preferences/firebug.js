f71c113b745a26c6e313c1f5b932b645116720eb
Sebastian Zartner <sebastianzartner@gmx.de>
2011-04-13 13:30:42 +0000

    [1.8] Added hint to preferences page in wiki and removed preference descriptions from file (see issue 4142)
    http://code.google.com/p/fbug/source/detail?r=10042

--- extension/defaults/preferences/firebug.js
+++ extension/defaults/preferences/firebug.js
@@ -1,3 +1,5 @@
+// For a detailed description of all preferences see http://getfirebug.com/wiki/index.php/Firebug_Preferences
+
 // Global
 pref("extensions.firebug.architecture", "inProcess");
 
@@ -10,17 +12,17 @@ pref("extensions.firebug.textWrapWidth", 100);
 pref("extensions.firebug.openInWindow", false);
 pref("extensions.firebug.previousPlacement", 0);
 pref("extensions.firebug.showErrorCount", true);
-pref("extensions.firebug.viewPanelOrient", false);          // true == vertical, false == horizontal
+pref("extensions.firebug.viewPanelOrient", false);
 pref("extensions.firebug.allPagesActivation", "none");
-pref("extensions.firebug.hiddenPanels", "");                // List of hidden panels separated by a space
-pref("extensions.firebug.panelTabMinWidth", 50);            // Minimum width of a panel tab [px] applied when there is no enough horizontal size for the tab bar.
-pref("extensions.firebug.sourceLinkLabelWidth", 17);        // Maximum amount of characters for source link labels (displayed e.g. in CSS or Console panels)
-pref("extensions.firebug.currentVersion", "");              // If Firebug version is bigger than the one in this string, a first-run welcome page is displayed.
-pref("extensions.firebug.useDefaultLocale", false);         // Set to true if Firebug should use default (en-US) locale instead of the current Firefox locale.
+pref("extensions.firebug.hiddenPanels", "");
+pref("extensions.firebug.panelTabMinWidth", 50);
+pref("extensions.firebug.sourceLinkLabelWidth", 17);
+pref("extensions.firebug.currentVersion", "");
+pref("extensions.firebug.useDefaultLocale", false);
 pref("extensions.firebug.activateSameOrigin", true);
-pref("extensions.firebug.toolbarCustomizationDone", false);  // Set to true if the start-button has been initially appended into Firefox toolbar.
-pref("extensions.firebug.addonBarOpened", false);            // Set to true if Firefox addon-bar has been initially opened for the user (to see the status bar icon at least the first time).
-pref("extensions.firebug.showBreakNotification", true);      // If true, Firebug shows a notification message when break in debugger happens (break on debugger; keyword, break on an error, etc.)
+pref("extensions.firebug.toolbarCustomizationDone", false);
+pref("extensions.firebug.addonBarOpened", false);
+pref("extensions.firebug.showBreakNotification", true);
 
 // Command line
 pref("extensions.firebug.largeCommandLine", false);
@@ -47,7 +49,7 @@ pref("extensions.firebug.showXMLHttpRequests", true);
 pref("extensions.firebug.showStackTrace", false);
 pref("extensions.firebug.console.logLimit", 500);
 pref("extensions.firebug.console.enableSites", false);
-pref("extensions.firebug.tabularLogMaxHeight", 200);      // Max height [px] for tabular output in console (provided e.g. by console.table() method). Set to zero in case of no limit.
+pref("extensions.firebug.tabularLogMaxHeight", 200);
 pref("extensions.firebug.consoleFilterTypes", "all");
 
 // HTML
@@ -60,7 +62,7 @@ pref("extensions.firebug.expandMutations", false);
 pref("extensions.firebug.scrollToMutations", false);
 pref("extensions.firebug.shadeBoxModel", true);
 pref("extensions.firebug.showQuickInfoBox", false);
-pref("extensions.firebug.displayedAttributeValueLimit", 1024); // Maximum characteres used to displaye value of an (HTML) attribute. There is no limit if set to 0.
+pref("extensions.firebug.displayedAttributeValueLimit", 1024);
 
 // CSS
 pref("extensions.firebug.onlyShowAppliedStyles", false);
@@ -68,7 +70,7 @@ pref("extensions.firebug.showUserAgentCSS", false);
 pref("extensions.firebug.expandShorthandProps", false);
 pref("extensions.firebug.showMozillaSpecificStyles", false);
 pref("extensions.firebug.computedStylesDisplay", "grouped");
-pref("extensions.firebug.cssEditMode", "Source");     // 'Source' == Source editing, 'Live' == Live editing
+pref("extensions.firebug.cssEditMode", "Source");
 
 // Script
 pref("extensions.firebug.breakOnErrors", false);
@@ -76,9 +78,6 @@ pref("extensions.firebug.showAllSourceFiles", false);
 pref("extensions.firebug.trackThrowCatch", false);
 pref("extensions.firebug.script.enableSites", false);
 pref("extensions.firebug.scriptsFilter", "all");
-
-// If the value is greather than zero, the Script panel replaces tabs
-// by corresponding number of spaces when displaying JavaScript source.
 pref("extensions.firebug.replaceTabs", 4);
 pref("extensions.firebug.filterSystemURLs", true);
 
@@ -100,22 +99,22 @@ pref("extensions.firebug.showRulers", true);
 pref("extensions.firebug.netFilterCategory", "all");
 pref("extensions.firebug.net.logLimit", 500);
 pref("extensions.firebug.net.enableSites", false);
-pref("extensions.firebug.netDisplayedResponseLimit", 102400); // Maximum size limit for displayed responses [net, console panels].
-pref("extensions.firebug.netDisplayedPostBodyLimit", 10240); // Maximum size limit for displayed post data source [net, console panels].
-pref("extensions.firebug.net.hiddenColumns", "");   // List of hidden columns for the Net panel (space separated)
-pref("extensions.firebug.netPhaseInterval", 1000);    // Specifies an interval (ms) after which a new phase (session) in the timeline graph is started. Set to 0 to not start new phase at all.
-pref("extensions.firebug.sizePrecision", 1);       // Number of displayed decimal places for size info in the UI. Allowed values: [-1 (all in bytes), 0 (no decimal places), 1 (one decimal place), 2 (two decimal places)]
-pref("extensions.firebug.netParamNameLimit", 25);       // Maximum size [characters] of displayed parameter names in the Net panel (post tab). No limit if zero or less.
+pref("extensions.firebug.netDisplayedResponseLimit", 102400);
+pref("extensions.firebug.netDisplayedPostBodyLimit", 10240);
+pref("extensions.firebug.net.hiddenColumns", "");
+pref("extensions.firebug.netPhaseInterval", 1000);
+pref("extensions.firebug.sizePrecision", 1);
+pref("extensions.firebug.netParamNameLimit", 25);
 pref("extensions.firebug.netShowPaintEvents", false);
-pref("extensions.firebug.netShowBFCacheResponses", true);   // Show responses coming from the BF (back-forward) cache. These doesn't represent any network activity.
-pref("extensions.firebug.netHtmlPreviewHeight", 100);       // Default height of a preview for net HTML responses.
+pref("extensions.firebug.netShowBFCacheResponses", true);
+pref("extensions.firebug.netHtmlPreviewHeight", 100);
 
 // JSON Preview
-pref("extensions.firebug.sortJsonPreview", false);   // If true JSON preview in the Net panel is sorted by keys.
+pref("extensions.firebug.sortJsonPreview", false);
 
 // Cache
-pref("extensions.firebug.cache.mimeTypes", ""); // list of additional cached mime-types separated by space.
-pref("extensions.firebug.cache.responseLimit", 5242880); // maximum size limit for cached response.
+pref("extensions.firebug.cache.mimeTypes", "");
+pref("extensions.firebug.cache.responseLimit", 5242880);
 
 // External Editors
 pref("extensions.firebug.externalEditors", "");
