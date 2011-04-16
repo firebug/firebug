310b77bdbed1e2aed3d3632a67f1336e8c35a76d
Harutyun Amirjanyan <amirjanyan@gmail.com>
2011-04-16 19:10:23 +0000

    [1.8 &1.7] Issue 4367: Last external editor still appearing in menu after being removed
    http://code.google.com/p/fbug/source/detail?r=10084

--- extension/content/firebug/external/externalEditors.js
+++ extension/content/firebug/external/externalEditors.js
@@ -120,25 +120,26 @@ Firebug.ExternalEditors = extend(Firebug.Module,
     onEditorsShowing: function(popup)
     {
         var editors = this.getRegisteredEditors();
-        if ( editors.length > 0 )
+
+        FBL.eraseNode(popup);
+        for( var i = 0; i < editors.length; ++i )
         {
-            var lastChild = popup.lastChild;
-            FBL.eraseNode(popup);
-            for( var i = 0; i < editors.length; ++i )
+            if (editors[i] == "-")
             {
-                if (editors[i] == "-")
-                {
-                    FBL.createMenuItem(popup, "-");
-                    continue;
-                }
-                var item = {label: editors[i].label, image: editors[i].image,
-                                nol10n: true };
-                var menuitem = FBL.createMenuItem(popup, item);
-                menuitem.value = editors[i].id;
+                FBL.createMenuItem(popup, "-");
+                continue;
             }
-            FBL.createMenuItem(popup, "-");
-            popup.appendChild(lastChild);
+            var item = {label: editors[i].label, image: editors[i].image,
+                        nol10n: true };
+            var menuitem = FBL.createMenuItem(popup, item);
+            menuitem.value = editors[i].id;
         }
+        if ( editors.length > 0 )
+            FBL.createMenuItem(popup, "-");
+        FBL.createMenuItem(popup, {
+            label: FBL.$STR('firebug.Configure_Editors'),
+            option: 'openEditorList'
+        });        
     },
 
     openEditorList: function()
@@ -174,7 +175,8 @@ Firebug.ExternalEditors = extend(Firebug.Module,
             this.appendContextMenuItem(popup, target.innerHTML, target.getAttribute("lineNumber"));
     },
 
-    createContextMenuItem: function(doc){
+    createContextMenuItem: function(doc)
+    {
         var item = doc.createElement('menu');
         item.setAttribute('type', "splitmenu");
         item.setAttribute('iconic', "true");
@@ -209,7 +211,7 @@ Firebug.ExternalEditors = extend(Firebug.Module,
 
     onContextMenuCommand: function(event)
     {
-        if (event.target.hasAttribute('openEditorList'))
+        if (event.target.getAttribute('option') == 'openEditorList')
             this.openEditorList();
         else if(event.currentTarget.hasAttribute('openFromContext'))
             this.openContext(Firebug.currentContext, event.target.value);
