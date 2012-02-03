0a7b9ce89d3a3536b1ad830cc68f66565b7b4ab3
Sebastian Zartner <sebastianzartner@gmx.de>
2012-02-03 12:11:40 +0000

    [1.10][&1.9] Issue 5205 ("Enable JavaScript and Refresh" doesn't work) http://code.google.com/p/fbug/issues/detail?id=5205
    http://code.google.com/p/fbug/source/detail?r=12808

--- extension/content/firebug/js/scriptPanel.js
+++ extension/content/firebug/js/scriptPanel.js
@@ -1774,7 +1774,7 @@ Firebug.ScriptPanel.WarningRep = domplate(Firebug.Rep,
     {
         Firebug.Options.setPref("javascript", "enabled", true);
 
-        Firebug.TabWatcher.reloadPageFromMemory(this.context);
+        Firebug.TabWatcher.reloadPageFromMemory(Firebug.currentContext);
     },
 
     onFocusDebugger: function(event)
