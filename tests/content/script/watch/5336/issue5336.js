1118bed1efe8ac8b020f983112f628fd6fb2907b
Jan Odvarko <odvarko@gmail.com>
2012-03-16 17:07:17 +0100

    [tests] Automated test for issue 5336

---
+++ tests/content/script/watch/5336/issue5336.js
@@ -0,0 +1,29 @@
+function runTest()
+{
+    FBTest.sysout("issue5336.START");
+    FBTest.openNewTab(basePath + "script/watch/5336/issue5336.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableScriptPanel(function(win)
+        {
+            var panel = FW.Firebug.chrome.selectPanel("script");
+
+            FBTest.waitForBreakInDebugger(FW.Firebug.chrome, 13, false, function(row)
+            {
+                FW.Firebug.chrome.selectSidePanel("watches");
+
+                var row = FBTest.getWatchExpressionRow(null, "elements");
+                FBTest.ok(row, "The 'elements' expression must be in the watch panel.");
+
+                var expected = /\s*elements\[\s*div\.test\,\s*div\.test\s*\]\s*/;
+                FBTest.compare(expected, row.textContent,
+                    "Value of 'elements' must not be undefined.");
+
+                FBTest.clickContinueButton();
+                FBTest.testDone("issue5336.DONE");
+            });
+
+            FBTest.click(win.document.getElementById("testButton"));
+        });
+    });
+}
