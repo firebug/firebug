66fedc5e4f3a259e2e3d47836d2df6bfbfe36ad3
sebastianz <sebastianzartner@gmx.de>
2012-04-02 10:46:02 +0200

    FBTest for issue 4805 (Distinguish or Highlight HTTPS protocol requests in Net panel) http://code.google.com/p/fbug/issues/detail?id=4805

---
+++ tests/content/net/4805/issue4805.js
@@ -0,0 +1,40 @@
+function runTest()
+{
+    FBTest.sysout("issue4805.START");
+
+    FBTest.openNewTab(basePath + "net/4805/issue4805.html", function(win)
+    {
+        FBTest.openFirebug();
+
+        FBTest.enableNetPanel(function(win)
+        {
+            var panel = FBTest.selectPanel("net");
+            var netPanelHeader = panel.panelNode.getElementsByClassName("netHeaderCell").item(0);
+            FBTest.executeContextMenuCommand(netPanelHeader, {label: "Protocol"}, function()
+            {
+                panel.clear();
+  
+                var options =
+                {
+                    tagName: "tr",
+                    classes: "netRow hasHeaders loaded",
+                    counter: 2
+                };
+
+                FBTest.waitForDisplayedElement("net", options, function(row)
+                {
+                    var panelNode = FBTest.selectPanel("net").panelNode;
+                    var urls = panelNode.getElementsByClassName("netFullHrefLabel");
+                    var protocols = panelNode.getElementsByClassName("netProtocolLabel");
+
+                    for (var i=0; i<protocols.length; i++)
+                        FBTest.compare(urls[i].textContent.replace(/^(.*?):.*/, "$1"), protocols[i].textContent, "The protocol of the "+(i+1)+". request must be correctly displayed");
+  
+                    FBTest.testDone("issue4805.DONE");
+                });
+  
+                FBTest.reload();
+            });
+        });
+    });
+}
