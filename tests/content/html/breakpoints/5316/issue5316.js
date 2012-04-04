be20339fa29f9d8dbed52c7dc009f591e208a616
Jan Odvarko <odvarko@gmail.com>
2012-04-04 19:16:45 +0200

    [tests] FBTest for issue 5316

---
+++ tests/content/html/breakpoints/5316/issue5316.js
@@ -0,0 +1,52 @@
+function runTest()
+{
+    FBTest.sysout("issue5136.START");
+    FBTest.openNewTab(basePath + "html/breakpoints/5316/issue5316.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableAllPanels();
+
+        // Reload window to activate debugger
+        FBTest.reload(function(win)
+        {
+            var chrome = FW.Firebug.chrome;
+            var content = win.document.getElementById("content");
+            var context = chrome.window.Firebug.currentContext;
+            var BP_BREAKONATTRCHANGE = 1;
+
+            // Set breakpoint.
+            FBTest.selectPanel("html");
+            FW.Firebug.HTMLModule.MutationBreakpoints.onModifyBreakpoint(context,
+                content, BP_BREAKONATTRCHANGE);
+
+            // Select the Script panel and cause Firebug to break.
+            FBTest.selectPanel("script");
+            breakOnMutation(win, function()
+            {
+                // Reload and cause break again.
+                FBTest.reload(function(win)
+                {
+                    breakOnMutation(win, function()
+                    {
+                        FBTest.testDone("issue5136.DONE");
+                    });
+                });
+            });
+        })
+    });
+}
+
+function breakOnMutation(win, callback)
+{
+    var chrome = FW.Firebug.chrome;
+    FBTest.waitForBreakInDebugger(chrome, 20, false, function(sourceRow)
+    {
+        FBTest.sysout("issue5136; before continue");
+        FBTest.clickContinueButton(chrome);
+        FBTest.progress("The continue button is pushed");
+        callback();
+    });
+
+    FBTest.click(win.document.getElementById("testButton"));
+    FBTest.sysout("button clicked");
+}
