firebug/firebug

commit 78cfd701c83cb78e695584999fc1ae7aa6cabe60

Introduced two new events APIs isSingleClick() and isDoubleClick()

extension/content/firebug/css/cssPanel.js
@@ -679,7 +679,7 @@ Firebug.CSSStyleSheetPanel.prototype = Obj.extend(Firebug.Panel,
             return;
 
         // XXjoe Hack to only allow clicking on the checkbox
-        if ((event.clientX <= 20) && (event.detail == 1))
+        if ((event.clientX <= 20) && !Events.isSingleClick(event))
         {
             if (Css.hasClass(event.target, "textEditor inlineExpander"))
                 return;
@@ -691,7 +691,7 @@ Firebug.CSSStyleSheetPanel.prototype = Obj.extend(Firebug.Panel,
                 Events.cancelEvent(event);
             }
         }
-        else if ((event.clientX >= 20) && (event.detail == 2))
+        else if ((event.clientX >= 20) && Events.isDoubleClick(event))
         {
             row = Dom.getAncestorByClass(event.target, "cssRule");
             if (row && !Dom.getAncestorByClass(event.target, "cssPropName")

extension/content/firebug/dom/domPanel.js
@@ -169,7 +169,7 @@ const DirTablePlate = domplate(Firebug.Rep,
                 Firebug.chrome.select(object, "script");
                 Events.cancelEvent(event);
             }
-            else if (event.detail == 2 && !object)
+            else if (Events.isDoubleClick(event) && !object)
             {
                 var panel = row.parentNode.parentNode.domPanel;
                 if (panel)

extension/content/firebug/html/htmlPanel.js
@@ -1009,7 +1009,7 @@ Firebug.HTMLPanel.prototype = Obj.extend(WalkingPanel,
 
     onClick: function(event)
     {
-        if (Events.isLeftClick(event) && event.detail == 2)
+        if (Events.isLeftClick(event) && Events.isDoubleClick(event))
         {
             // The double-click (detail == 2) expands an HTML element, but the user must click
             // on the element itself not on the twisty.
@@ -1020,7 +1020,7 @@ Firebug.HTMLPanel.prototype = Obj.extend(WalkingPanel,
             if (!Css.hasClass(event.target, "twisty") && !Css.hasClass(event.target, "nodeLabel"))
                 this.toggleNode(event);
         }
-        else if (Events.isAltClick(event) && event.detail == 2 && !this.editing)
+        else if (Events.isAltClick(event) && Events.isDoubleClick(event) && !this.editing)
         {
             var node = Firebug.getRepObject(event.target);
             this.editNode(node);

extension/content/firebug/lib/events.js
@@ -161,8 +161,17 @@ Events.isMiddleClick = function(event, allowKeyModifiers)
 
 Events.isRightClick = function(event, allowKeyModifiers)
 {
-
     return event.button == 2 && (allowKeyModifiers || this.noKeyModifiers(event));
+};
+
+Events.isSingleClick = function(event)
+{
+    return event instanceof MouseEvent && event.detail == 1;
+};
+
+Events.isDoubleClick = function(event)
+{
+    return event instanceof MouseEvent && event.detail == 2;
 };
 
 Events.noKeyModifiers = function(event)

commit 6120b1ba068cd9849d06a8ee7052e8935c1fb1b8

Fixed style disabling problem introduced with 78cfd701c8.

extension/content/firebug/css/cssPanel.js
@@ -679,7 +679,7 @@ Firebug.CSSStyleSheetPanel.prototype = Obj.extend(Firebug.Panel,
             return;
 
         // XXjoe Hack to only allow clicking on the checkbox
-        if ((event.clientX <= 20) && !Events.isSingleClick(event))
+        if ((event.clientX <= 20) && Events.isSingleClick(event))
         {
             if (Css.hasClass(event.target, "textEditor inlineExpander"))
                 return;