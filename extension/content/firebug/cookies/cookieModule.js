93b7cce030a70b2d05668f38e1625debc06483bb
Jan Odvarko <odvarko@gmail.com>
2012-06-08 14:41:46 +0200

    Properly localize Cookies panel using fbInternational

--- extension/content/firebug/cookies/cookieModule.js
+++ extension/content/firebug/cookies/cookieModule.js
@@ -178,24 +178,6 @@ Firebug.CookieModule = Obj.extend(Firebug.ActivableModule,
         //Firebug.Debugger.removeListener(this.DebuggerListener);
     },
 
-    internationalizeUI: function()
-    {
-        var elements = ["fcCookiesMenu", "fcExportAll", "fcExportForSite", "fcRemoveAllSession",
-            "fcRemoveAll", "fcCreate", "fcCookieViewAll", "fcCookieViewExceptions",
-            "fcToolsMenu", "fcFilterMenu", "fcFilterByPath",
-            "fcShowRejectedCookies", "fbConsoleFilter-cookies"];
-
-        for (var i=0; i<elements.length; i++)
-        {
-            var element = Firebug.chrome.$(elements[i]);
-            if (element.hasAttribute("label"))
-                Locale.internationalize(element, "label");
-
-            if (element.hasAttribute("tooltiptext"))
-                Locale.internationalize(element, "tooltiptext");
-        }
-    },
-
     registerObservers: function()
     {
         if (this.observersRegistered)
