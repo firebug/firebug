f89f8509199a039c5189e84aa74b43fd7dba0a26
Jan Odvarko <odvarko@gmail.com>
2012-06-13 18:01:43 +0200

    Cookies: support for batch-deleted event

--- extension/content/firebug/cookies/cookieObserver.js
+++ extension/content/firebug/cookies/cookieObserver.js
@@ -53,10 +53,29 @@ var CookieObserver = Obj.extend(BaseObserver,
             if (!Firebug.CookieModule.isAlwaysEnabled())
                 return;
 
+            // See: https://developer.mozilla.org/en/XPCOM_Interface_Reference/nsICookieService
+            // For all possible values.
             if (aTopic == "cookie-changed")
             {
-                aSubject = aSubject ? aSubject.QueryInterface(Ci.nsICookie2) : null;
-                this.iterateContexts(this.onCookieChanged, aSubject, aData);
+                var cookies = []
+                if (aData == "batch-deleted")
+                {
+                    // In this case the subject is nsIArray.
+                    var enumerator = aSubject.QueryInterface(Ci.nsIArray).enumerate();
+                    while (enumerator.hasMoreElements())
+                        cookies.push(enumerator.getNext().QueryInterface(Ci.nsICookie2));
+
+                    // The event will be further distributed as standard "delete" event.
+                    aData = "deleted";
+                }
+                else
+                {
+                    aSubject = aSubject ? aSubject.QueryInterface(Ci.nsICookie2) : null;
+                    cookies.push(aSubject);
+                }
+
+                for (var i=0; i<cookies.length; i++)
+                    this.iterateContexts(this.onCookieChanged, cookies[i], aData);
             }
             else if (aTopic == "cookie-rejected")
             {
@@ -67,7 +86,11 @@ var CookieObserver = Obj.extend(BaseObserver,
         catch (err)
         {
             if (FBTrace.DBG_ERRORS)
-                FBTrace.sysout("cookies.CookieObserver.observe ERROR " + aTopic, err);
+            {
+                FBTrace.sysout("cookies.CookieObserver.observe; ERROR " +
+                    aTopic + ", " + aData, err);
+                FBTrace.sysout("cookies.CookieObserver.observe; subject ", aSubject);
+            }
         }
     },
 
@@ -222,6 +245,9 @@ var CookieObserver = Obj.extend(BaseObserver,
             break;
           case "cleared":
             this.onClear(context);
+            return;
+          case "reload":
+            context.invalidatePanels(panelName);
             return;
         }
     },
