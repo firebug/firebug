39ba11f17fac08503efa9d6240138599d2ca8d48
Jan Odvarko <odvarko@gmail.com>
2012-06-18 17:48:04 +0200

    Issue 5578: 	First-run page is just shown on browser restart

--- extension/content/firebug/firefox/browserOverlay.js
+++ extension/content/firebug/firefox/browserOverlay.js
@@ -1153,7 +1153,6 @@ if (version)
 // ********************************************************************************************* //
 // First Run Page
 
-var observerService = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
 var wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
 
 function checkFirebugVersion(currentVersion)
@@ -1169,39 +1168,23 @@ function checkFirebugVersion(currentVersion)
     return versionChecker.compare(version, currentVersion);
 }
 
-var SessionObserver =
+if (checkFirebugVersion(PrefLoader.getPref("currentVersion")) > 0)
 {
-    observe: function(subjet, topic, data)
+    // Open the page in the top most window, so the user can see it immediately.
+    if (wm.getMostRecentWindow("navigator:browser") == window.top)
     {
-        if (topic != "sessionstore-windows-restored")
-            return;
+        // Don't forget to update the preference, so the page is not displayed again
+        PrefLoader.setPref("currentVersion", version);
 
-        //xxxHonza: Removing observer at this moment is risky. What if the registration
-        // is done too late and the even never come?
-        observerService.removeObserver(SessionObserver, "sessionstore-windows-restored");
-
-        setTimeout(function()
+        if (PrefLoader.getPref("showFirstRunPage"))
         {
-            // Open the page in the top most window, so the user can see it immediately.
-            if (wm.getMostRecentWindow("navigator:browser") != window.top)
-                return;
-
-            // Avoid opening of the page in another browser window.
-            if (checkFirebugVersion(PrefLoader.getPref("currentVersion")) > 0)
-            {
-                // Don't forget to update the preference, so the page is not displayed again
-                PrefLoader.setPref("currentVersion", version);
-
-                if (PrefLoader.getPref("showFirstRunPage"))
-                    Firebug.GlobalUI.visitWebsite("firstRunPage",  version);
-            }
-        }, 500);
+            setTimeout(function() {
+                var version = Firebug.GlobalUI.getVersion();
+                Firebug.GlobalUI.visitWebsite("firstRunPage", version);
+            }, 100);
+        }
     }
 }
-
-var currentVersion = PrefLoader.getPref("currentVersion");
-if (checkFirebugVersion(currentVersion) > 0)
-    observerService.addObserver(SessionObserver, "sessionstore-windows-restored", false);
 
 // ********************************************************************************************* //
 // Firefox Page Context Menu
