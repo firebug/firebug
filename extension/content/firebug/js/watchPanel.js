fa533c0105fa5a33a06aafc3fdec39c303aef25c
Sebastian Zartner <sebastianzartner@gmx.de>
2012-07-01 01:38:12 +0200

    Trim watch expressions before adding them

--- extension/content/firebug/js/watchPanel.js
+++ extension/content/firebug/js/watchPanel.js
@@ -9,9 +9,10 @@ define([
     "firebug/lib/css",
     "firebug/js/stackFrame",
     "firebug/lib/locale",
+    "firebug/lib/string",
     "firebug/dom/domPanel",     // Firebug.DOMBasePanel, Firebug.DOMPanel.DirTable
 ],
-function(Obj, Firebug, ToggleBranch, Events, Dom, Css, StackFrame, Locale) {
+function(Obj, Firebug, ToggleBranch, Events, Dom, Css, StackFrame, Locale, Str) {
 
 // ********************************************************************************************* //
 // Watch Panel
@@ -193,6 +194,8 @@ Firebug.WatchPanel.prototype = Obj.extend(Firebug.DOMBasePanel.prototype,
 
     addWatch: function(expression)
     {
+        expression = Str.trim(expression);
+
         if (FBTrace.DBG_WATCH)
             FBTrace.sysout("Firebug.WatchPanel.addWatch; expression: "+expression);
 
