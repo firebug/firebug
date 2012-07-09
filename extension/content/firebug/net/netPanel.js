61bff289c6244094621b67c31470e35194c76b8c
Sebastian Zartner <sebastianzartner@gmx.de>
2012-07-09 23:31:08 +0200

    Fixed line breaks between request parameters for different OSes

--- extension/content/firebug/net/netPanel.js
+++ extension/content/firebug/net/netPanel.js
@@ -476,7 +476,7 @@ NetPanel.prototype = Obj.extend(Firebug.ActivablePanel,
     {
         var params = Url.parseURLParams(file.href);
         var result = params.map(function(o) { return o.name + ": " + o.value; });
-        System.copyToClipboard(result.join("\n"));
+        System.copyToClipboard(result.join(Str.lineBreak()));
     },
 
     copyPOSTParams: function(file)
