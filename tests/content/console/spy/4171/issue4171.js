dcbe48713d2fa0a90df7825d2aa58719c1688200
Jan Odvarko <odvarko@gmail.com>
2012-07-18 13:19:38 +0200

    FBTest for issue 4171

---
+++ tests/content/console/spy/4171/issue4171.js
@@ -0,0 +1,45 @@
+function runTest()
+{
+    FBTest.sysout("issue4171.START");
+    FBTest.setPref("showXMLHttpRequests", true);
+    FBTest.openNewTab(basePath + "console/spy/4171/issue4171.html", function(win)
+    {
+        // Open Firebug and enable the Console panel.
+        FBTest.openFirebug();
+        FBTest.enableConsolePanel(function(win)
+        {
+            var options = {
+                tagName: "div",
+                classes: "logRow logRow-spy loading"
+            };
+
+            // Wait till the XHR request in the Console appear (loading in progress)
+            FBTest.waitForDisplayedElement("console", options, function(row)
+            {
+                var clickTarget = row.getElementsByClassName("spyTitleCol spyCol")[0];
+                FBTest.click(clickTarget);
+
+                // At this point, there is only Header tab.
+                var tab = row.getElementsByClassName("netInfoHeadersText netInfoText")[0];
+                FBTest.ok(tab, "Headers tab must exist");
+
+                // Wait till the XHR request finishes (changes its state to loaded)
+                options.classes = "logRow logRow-spy loaded";
+                FBTest.waitForDisplayedElement("console", options, function(row)
+                {
+                    // Now there must be both: the Response and JSON tab.
+                    var tab = row.getElementsByClassName("netInfoResponseText netInfoText")[0];
+                    FBTest.ok(tab, "Response tab must exist");
+
+                    tab = row.getElementsByClassName("netInfoJSONText netInfoText")[0];
+                    FBTest.ok(tab, "JSON tab must exist");
+
+                    FBTest.testDone("issue2868.DONE");
+                });
+            });
+
+            // Execute test implemented on the test page.
+            FBTest.click(win.document.getElementById("testButton"));
+        });
+    });
+}
