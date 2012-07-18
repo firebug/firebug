af6e178579cc3049d3f3afcb06abaa199a619b9d
Jan Odvarko <odvarko@gmail.com>
2012-07-18 17:37:38 +0200

    Quick fix of getMessageId() for groupped logging, should be yet verified

--- extension/content/firebug/console/consolePanel.js
+++ extension/content/firebug/console/consolePanel.js
@@ -439,6 +439,8 @@ Firebug.ConsolePanel.prototype = Obj.extend(Firebug.ActivablePanel,
             var previousMsgId = this.lastLogRow ?
                 this.getMessageId(this.lastLogRow.objects, this.lastLogRow.sourceLink) : "";
 
+FBTrace.sysout("previousMsgId " + previousMsgId + ", " + msgId);
+
             if (msgId && msgId == previousMsgId)
             {
                 this.increaseRowCount(container.lastChild);
