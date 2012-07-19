d889f727dd281191ab30685c6a1cbf34ac2f51b5
Sebastian Zartner <sebastianzartner@gmx.de>
2012-07-19 23:21:04 +0200

    Backported from master (8548b0d2496aedc964b957d4a2fa655420fe9815)
    Issue 5341 (Open in new tab via middle mouse click is not working)
    http://code.google.com/p/fbug/issues/detail?id=5341

--- extension/content/firebug/chrome/chrome.js
+++ extension/content/firebug/chrome/chrome.js
@@ -2039,19 +2039,6 @@ function onPanelClick(event)
                 }
             }
         }
-        else if (Events.isControlClick(event) || Events.isMiddleClick(event))
-        {
-            if (!realRep || !realRep.browseObject(realObject, Firebug.currentContext))
-            {
-                if (rep && !(rep != realRep && rep.browseObject(object, Firebug.currentContext)))
-                {
-                    var panel = Firebug.getElementPanel(event.target);
-                    if (!panel || !panel.browseObject(realObject))
-                        return;
-                }
-            }
-            Events.cancelEvent(event);
-        }
     }
 }
 
@@ -2120,6 +2107,30 @@ function onPanelMouseUp(event)
                 Events.cancelEvent(event);
             }
         }
+    }
+    else if (Events.isControlClick(event) || Events.isMiddleClick(event))
+    {
+        var repNode = Firebug.getRepNode(event.target);
+        if (!repNode)
+            return;
+
+        var object = repNode.repObject;
+        var rep = Firebug.getRep(object, Firebug.currentContext);
+        var realObject = rep ? rep.getRealObject(object, Firebug.currentContext) : null;
+        var realRep = realObject ? Firebug.getRep(realObject, Firebug.currentContext) : rep;
+        if (!realObject)
+            realObject = object;
+
+        if (!realRep || !realRep.browseObject(realObject, Firebug.currentContext))
+        {
+            if (rep && !(rep != realRep && rep.browseObject(object, Firebug.currentContext)))
+            {
+                var panel = Firebug.getElementPanel(event.target);
+                if (!panel || !panel.browseObject(realObject))
+                    return;
+            }
+        }
+        Events.cancelEvent(event);
     }
 }
 
