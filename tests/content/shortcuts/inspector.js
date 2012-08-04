01dbe135d13727a0adc9ffefb5109d27ee1ed8df
Steven Roussey <sroussey@gmail.com>
2012-08-04 15:15:35 -0700

    Fix test shortcuts/inspector.js for MacOS

--- tests/content/shortcuts/inspector.js
+++ tests/content/shortcuts/inspector.js
@@ -6,13 +6,13 @@ function runTest()
         FBTest.ok(!FBTest.isFirebugActive(), "Firebug must be suspended");
 
         // Activate inspector
-        FBTest.sendShortcut("c", {shiftKey: true, ctrlKey: true});
+        FBTest.sendShortcut("c", {shiftKey: true, accelKey: true});
         FBTest.compare("inBrowser", FBTest.getFirebugPlacement(), "Firebug must be inBrowser");
         FBTest.ok(FBTest.isFirebugActive(), "Firebug must be active");
         FBTest.ok(FBTest.isInspectorActive(), "HTML Inspector must be inspecting");
 
         // Deactivate inspector
-        FBTest.sendShortcut("c", {shiftKey: true, ctrlKey: true});
+        FBTest.sendShortcut("c", {shiftKey: true, accelKey: true});
         FBTest.compare("inBrowser", FBTest.getFirebugPlacement(), "Firebug must be inBrowser");
         FBTest.ok(FBTest.isFirebugActive(), "Firebug must be active");
         FBTest.ok(!FBTest.isInspectorActive(), "HTML Inspector must be deactivated");
