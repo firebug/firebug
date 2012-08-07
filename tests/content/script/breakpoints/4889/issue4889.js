921595662725ce8a7d04bc55e068750f12f028eb
Sebastian Zartner <sebastianzartner@gmx.de>
2012-08-07 09:56:39 +0200

    FBTest for issue 4889 (Folding for breakpoint categories) http://code.google.com/p/fbug/issues/detail?id=4889

---
+++ tests/content/script/breakpoints/4889/issue4889.js
@@ -0,0 +1,57 @@
+function runTest()
+{
+    FBTest.sysout("issue4889.START");
+
+    FBTest.openNewTab(basePath + "script/breakpoints/4889/issue4889.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableScriptPanel();
+        FBTest.enableConsolePanel();
+
+        FBTest.selectPanel("console");
+
+        var config = {tagName: "div", classes: "logRow logRow-errorMessage", count: 2};
+        FBTest.waitForDisplayedElement("console", config, function(row)
+        {
+            var panelNode = FW.FBL.getAncestorByClass(row, "panelNode");
+            var errorMessages = panelNode.getElementsByClassName("logRow-errorMessage");
+
+            for (var i = 0; i < errorMessages.length; ++i)
+            {
+                var breakpoint = errorMessages[i].getElementsByClassName("errorBreak").item(0);
+                var errorMessage = errorMessages[i].getElementsByClassName("errorMessage").item(0);
+                if (FBTest.ok(breakpoint, "There must be an breakpoint available for the error " +
+                    "log '" + errorMessage.textContent +"'."))
+                {
+                    FBTest.click(breakpoint);
+                }
+            }
+
+            FBTest.selectPanel("script");
+            panelNode = FBTest.selectPanel("breakpoints").panelNode;
+            var breakpointHeader = panelNode.getElementsByClassName("breakpointHeader").item(0);
+
+            if (FBTest.compare("Error Breakpoints",
+                breakpointHeader && breakpointHeader.textContent,
+                "There must be a breakpoint category 'Error Breakpoints'"))
+            {
+                var breakpointBlock = panelNode.getElementsByClassName("breakpointBlock").item(0);
+
+                FBTest.click(breakpointHeader);
+
+                FBTest.ok(!FW.FBL.hasClass(breakpointBlock, "opened"),
+                    "The breakpoint category should be collapsed.");
+
+                FBTest.click(breakpointHeader);
+                FBTest.ok(FW.FBL.hasClass(breakpointBlock, "opened"),
+                    "The breakpoint category should be expanded.");
+            }
+
+            // Finish test
+            FBTest.testDone("issue4889.DONE");
+        });
+
+        FBTest.click(win.document.getElementById("callFirstFunction"));
+        FBTest.click(win.document.getElementById("callSecondFunction"));
+    });
+}
