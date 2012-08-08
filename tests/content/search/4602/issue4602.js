d334ea044485386187ebe1101126f3c7c857801b
Sebastian Zartner <sebastianzartner@gmx.de>
2012-08-08 09:52:45 +0200

    FBTest for issue 4602 (Transfer text selection to Search Field when it's focussed using the keyboard shortcut) http://code.google.com/p/fbug/issues/detail?id=4602

---
+++ tests/content/search/4602/issue4602.js
@@ -0,0 +1,31 @@
+function runTest()
+{
+    FBTest.sysout("issue4602.START");
+
+    FBTest.openNewTab(basePath + "search/4602/issue4602.html", function(win)
+    {
+        FBTest.openFirebug();
+        var panel = FBTest.selectPanel("stylesheet");
+        if (FBTest.ok(FBTest.selectPanelLocationByName(panel, "issue4602.html"),
+            "CSS Location Menu should contain an entry for 'issue4602.html'"))
+        {
+            var doc = panel.panelNode.ownerDocument;
+
+            // Set focus to Firebug, otherwise the selection and the shortcut will be sent to the browser document 
+            doc.documentElement.focus();
+
+            var range = doc.createRange();
+            var startNode = doc.getElementsByClassName("cssPropName").item(0);
+            range.setStart(startNode, 0);
+            var endNode = doc.getElementsByClassName("cssPropValue").item(0);
+            range.setEnd(endNode, 1);
+            doc.getSelection().addRange(range);
+
+            FBTest.sendShortcut("F", {ctrlKey: true});
+            FBTest.compare("color: blue", FBTest.getSearchFieldText(),
+                "The value inside the search field much match the selection.");
+        }
+
+        FBTest.testDone("issue4602.DONE");
+    });
+}
