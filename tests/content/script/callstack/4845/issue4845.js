b9ed936de5871c924e577c8b5e95f65886c60c2c
Sebastian Zartner <sebastianzartner@gmx.de>
2012-08-08 07:17:57 +0200

    FBTest for issue 4845 (Rerun should have a shortcut like the other debugging options) http://code.google.com/p/fbug/issues/detail?id=4845

---
+++ tests/content/script/callstack/4845/issue4845.js
@@ -0,0 +1,54 @@
+function runTest()
+{
+    FBTest.sysout("issue4845.START");
+
+    FBTest.openNewTab(basePath + "script/callstack/4845/issue4845.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableConsolePanel();
+        FBTest.enableScriptPanel();
+        FBTest.selectPanel("script");
+
+        FBTest.waitForBreakInDebugger(null, 11, false, function()
+        {
+            var panel = FBTest.selectPanel("console");
+            var logRows = panel.panelNode.getElementsByClassName("objectBox-text");
+            var logMessages = 0;
+            for (var i = 0; i < logRows.length; ++i)
+            {
+                if (logRows.item(i).textContent == "Hello Firebug user!")
+                    logMessages++;
+            }
+
+            FBTest.compare(1, logMessages,
+                "There must be one 'Hello Firebug user!' message logged to the console.");
+
+            panel = FBTest.selectPanel("script");
+
+            var config = {tagName: "div", classes: "logRow logRow-log", count: 2, onlyMutations: true};
+            FBTest.waitForDisplayedElement("console", config, function(row)
+            {
+                panel = FBTest.selectPanel("console");
+                logRows = panel.panelNode.getElementsByClassName("objectBox-text");
+
+                logMessages = 0;
+                for (var i = 0; i < logRows.length; ++i)
+                {
+                    if (logRows.item(i).textContent == "Hello Firebug user!")
+                        logMessages++;
+                }
+
+                FBTest.compare(2, logMessages,
+                    "There must be two 'Hello Firebug user!' messages logged to the console.");
+
+                FBTest.clickContinueButton();
+
+                FBTest.testDone("issue4845.DONE");
+            });
+
+            FBTest.sendShortcut("VK_F8", {shiftKey: true});
+        });
+
+        FBTest.reload();
+    });
+}
