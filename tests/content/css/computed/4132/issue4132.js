744bf0f17031944861f164b55932c5a76170b711
Sebastian Zartner <sebastianzartner@gmx.de>
2012-08-08 21:18:25 +0200

    FBTest for issue 4132 (Only show applied styles in Computed side panel) http://code.google.com/p/fbug/issues/detail?id=4132

---
+++ tests/content/css/computed/4132/issue4132.js
@@ -0,0 +1,25 @@
+function runTest()
+{
+    FBTest.sysout("issue4132.START");
+
+    FBTest.openNewTab(basePath + "css/computed/4132/issue4132.html", function(win)
+    {
+        FBTest.openFirebug();
+        var panel = FBTest.selectPanel("computed");
+
+        FBTest.selectElementInHtmlPanel("element", function(win)
+        {
+            var computedStyles = panel.panelNode.querySelectorAll(".computedStyle:not(.hasSelectors)");
+
+            FBTest.compare(0, computedStyles.length, "There should not be any user agent styles be shown.");
+
+            FBTest.setPref("showUserAgentCSS", true);
+
+            computedStyles = panel.panelNode.querySelectorAll(".computedStyle:not(.hasSelectors)");
+
+            FBTest.ok(computedStyles.length > 0, "There should be user agent styles shown.");
+
+            FBTest.testDone("issue4132.DONE");
+        });
+    });
+}
