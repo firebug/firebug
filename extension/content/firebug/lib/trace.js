c271f28d85ac837406687137573c9801031588b9
Jan Odvarko <odvarko@gmail.com>
2012-08-17 13:44:27 +0200

    Fix firebug/lib/trace so, tracing is not broken when requested before the tracing console is actually available

--- extension/content/firebug/lib/trace.js
+++ extension/content/firebug/lib/trace.js
@@ -26,10 +26,41 @@ catch (err)
             var TraceObj = {};
             for (var i=0; i<TraceAPI.length; i++)
                 TraceObj[TraceAPI[i]] = function() {};
+
+            // If the FBTrace object is requested too soon, when the Tracing Console
+            // doesn't yet exist, let's change that default sysout method so, it
+            // tries to get the object again.
+            TraceObj.sysout = function(msg)
+            {
+                try
+                {
+                    Cu.import("resource://fbtrace/firebug-trace-service.js", scope);
+                    var FBTrace = scope.traceConsoleService.getTracer("extensions.firebug");
+                    FBTrace.sysout.apply(FBTrace, arguments);
+                }
+                catch (err)
+                {
+                    //Cu.reportError(getStackDump());
+                    Cu.reportError(msg);
+                }
+
+            }
+
             return TraceObj;
         }
     };
 }
+
+// ********************************************************************************************* //
+
+function getStackDump()
+{
+    var lines = [];
+    for (var frame = Components.stack; frame; frame = frame.caller)
+        lines.push(frame.filename + " (" + frame.lineNumber + ")");
+
+    return lines.join("\n");
+};
 
 // ********************************************************************************************* //
 
