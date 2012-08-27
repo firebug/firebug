03d96c694fe60d124efeb7dbef70bb48cbbc4773
Jan Odvarko <odvarko@gmail.com>
2012-08-27 19:04:41 +0200

    FBTest for issue 5868

---
+++ tests/content/commandLine/api/dirxml.js
@@ -0,0 +1,20 @@
+function runTest()
+{
+    FBTest.sysout("commandline.dirxml.START");
+    FBTest.openNewTab(basePath + "commandline/api/dirxml.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableConsolePanel(function(win)
+        {
+            var config = {tagName: "div", classes: "logRow logRow-dirxml"};
+            FBTest.waitForDisplayedElement("console", config, function(row)
+            {
+                var xml = /\s*<div\s*id=\"content\"\s*style=\"display:\s*none;\"><span>a<\/span><span><span>b<\/span><\/span><\/div>/;
+                FBTest.compare(xml, row.textContent, "XML must be properly displayed.");
+                FBTest.testDone("commandline.dirxml.DONE");
+            });
+
+            FBTest.executeCommand("dirxml(document.getElementById('content'));");
+        });
+    });
+}
