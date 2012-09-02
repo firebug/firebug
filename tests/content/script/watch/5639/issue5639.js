7b39d56684e64351380eb7cd041a6be71b788273
Florent FAYOLLE <cold_sun@hotmail.fr>
2012-09-02 14:28:06 +0200

    FBTest for issue 5639

--- tests/content/script/watch/5639/issue5639.js
+++ tests/content/script/watch/5639/issue5639.js
@@ -4,22 +4,54 @@ function runTest()
     FBTest.openNewTab(basePath + "script/watch/5639/issue5639.html", function(win)
     {
         FBTest.openFirebug();
-        FBTest.sysout("test");
         FBTest.enableScriptPanel(function(win)
         {
-            try{
-                FBTest.selectPanel("script");
-                var panelNode = FBTest.selectSidePanel("watches");
-                FBTest.sysout(panelNode);
-                alert(panelNode);
-                FBTest.rightClick(panelNode, win);
-                /*panelNode.style.backgroundColor = "red";
-                FBTest.testDone("issue5639.DONE");*/
-            }
-            catch(ex)
+            FBTest.selectPanel("script");
+            var panelNode = FBTest.selectSidePanel("watches").panelNode;
+            var watchExpressions = ["a", "b"];
+            var tasks = new FBTest.TaskList();
+            // click on a watch expression:
+            tasks.push(testDeleteAllWatches, panelNode, ".memberValueCell", watchExpressions);
+            // click on the watch edition area
+            tasks.push(testDeleteAllWatches, panelNode, ".watchEditCell", watchExpressions);
+
+            tasks.run(function()
             {
-                FBTest.exception("issue5639 "+ex);
-            }
+                FBTest.testDone("issue5639.DONE");
+            });
         });
     });
 }
+
+function countWatches(panelNode)
+{
+    return panelNode.getElementsByClassName("memberValueCell").length;
+}
+
+function testDeleteAllWatches(callback, panelNode, targetSelector, watchExpressions)
+{
+    addWatches(watchExpressions.slice(0), function()
+    {
+        FBTest.compare(watchExpressions.length, countWatches(panelNode), "all the watches must be added");
+        var target = panelNode.querySelector(targetSelector);
+        FBTest.executeContextMenuCommand(target, "deleteAllWatches", function()
+        {
+            FBTest.compare(0, countWatches(panelNode), "There should not be any watch");
+            callback();
+        });
+    });
+}
+
+function addWatches(watchExpressions, callback)
+{
+    if (watchExpressions.length > 0)
+    {
+        FBTest.addWatchExpression(null, watchExpressions.pop(), function()
+        {
+            addWatches(watchExpressions, callback);
+        });
+    }
+    else
+        callback();
+}
+
