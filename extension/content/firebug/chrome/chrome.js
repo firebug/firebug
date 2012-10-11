7334c87bbbd5169a93a54b6aacd8a88b44299d7b
Jan Odvarko <odvarko@gmail.com>
2012-10-11 17:14:34 +0200

    Always use parent of the Firebug window (not top)

--- extension/content/firebug/chrome/chrome.js
+++ extension/content/firebug/chrome/chrome.js
@@ -1111,8 +1111,7 @@ var FirebugChrome =
         var vertical = pos == "top" || pos == "bottom";
         var after = pos == "bottom" || pos == "right";
 
-        // xxxHonza: shouldn't this be rather: window.parent.document?
-        var document = window.top.document;
+        var document = window.parent.document;
         var container = document.getElementById(vertical ? "appcontent" : "browser");
 
         var splitter = Firefox.getElementById("fbContentSplitter");
