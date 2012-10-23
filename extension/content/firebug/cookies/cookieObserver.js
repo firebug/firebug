2169423000f576cedf697ce73c41620396f95ccf
Jan Odvarko <odvarko@gmail.com>
2012-10-23 16:44:19 +0200

    Use Str.endsWith to simplify the code (related to issue 5964)

--- extension/content/firebug/cookies/cookieObserver.js
+++ extension/content/firebug/cookies/cookieObserver.js
@@ -130,25 +130,27 @@ var CookieObserver = Obj.extend(BaseObserver,
 
         // Get directory path (without the file name)
         var queryStringPos = activeUri.path.lastIndexOf("?");
-        var activePath = queryStringPos != -1 ?
-            activeUri.path.substr(0, queryStringPos) : activeUri.path;
 
-        //Steps 2,3 of RFC 6265 section 5.1.4
-        if (activePath.length == 0 || activePath.charAt(0) != "/" || activePath.lastIndexOf("/") == 0)
-            activePath = "/";
+        // Get the current active path.
+        var ap = queryStringPos != -1 ? activeUri.path.substr(0, queryStringPos) : activeUri.path;
+
+        // Steps 2,3 of RFC 6265 section 5.1.4
+        if (ap.length == 0 || ap.charAt(0) != "/" || ap.lastIndexOf("/") == 0)
+        {
+            ap = "/";
+        }
         else
-	{
-            // Remove slash at the end of the active path according to step 4 of RFC 6265 section 5.1.4
-            var lastChar = activePath.charAt(activePath.length - 1);
-            if (lastChar == "/")
-       	        activePath = activePath.substr(0, activePath.length - 1);
+        {
+            // Remove slash at the end of the active path according to
+            // step 4 of RFC 6265 section 5.1.4
+            if (Str.endsWith(ap, "/"))
+                ap = ap.substr(0, ap.length - 1);
         }
 
         // If the path filter is on, only cookies that match given path
         // according to RFC 6265 section 5.1.4 will be displayed.
-        if (pathFilter && (activePath != cookiePath && !(Str.hasPrefix(activePath, cookiePath) &&
-            (cookiePath.charAt(cookiePath.length - 1) == "/" ||
-                activePath.substr(cookiePath.length, 1) == "/"))))
+        if (pathFilter && (ap != cookiePath && !(Str.hasPrefix(ap, cookiePath) &&
+            (Str.endsWith(cookiePath, "/") || ap.substr(cookiePath.length, 1) == "/"))))
         {
             return false;
         }
