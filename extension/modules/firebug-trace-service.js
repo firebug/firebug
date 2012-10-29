8c4a2994fb3f43eb33ce3d50c6d8bf1f855cdd3a
Jan Odvarko <odvarko@gmail.com>
2012-10-29 18:21:06 +0100

    Fix tracing

--- extension/modules/firebug-trace-service.js
+++ extension/modules/firebug-trace-service.js
@@ -32,13 +32,25 @@ catch (err)
             for (var i=0; i<TraceAPI.length; i++)
                 TraceObj[TraceAPI[i]] = function() {};
 
+            var optionsSet = false;
+
             TraceObj.sysout = function(msg)
             {
                 try
                 {
+                    var scope = {};
                     Cu.import("resource://fbtrace/firebug-trace-service.js", scope);
                     var FBTrace = scope.traceConsoleService.getTracer("extensions.firebug");
                     FBTrace.sysout.apply(FBTrace, arguments);
+
+                    // Copy all options from real FBTrace object into the one that has
+                    // been already created.
+                    if (!optionsSet)
+                    {
+                        for (var p in FBTrace)
+                            TraceObj[p] = FBTrace[p];
+                        optionsSet = true;
+                    }
                 }
                 catch (err)
                 {
