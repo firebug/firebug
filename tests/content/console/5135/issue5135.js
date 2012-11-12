987d8a8ae9dd8afc26bedf5f8e9ed6d8dac4b191
Jan Odvarko <odvarko@gmail.com>
2012-11-12 16:05:33 +0100

    FBTest: automated test for issue 5135

---
+++ tests/content/console/5135/issue5135.js
@@ -0,0 +1,25 @@
+function runTest()
+{
+    FBTest.sysout("issue5135.START");
+    FBTest.openNewTab(basePath + "console/5135/issue5135.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableConsolePanel(function(win)
+        {
+            FBTest.selectPanel("console");
+            FBTest.executeCommand("monitorEvents($('iframe').contentWindow, 'message')");
+            FBTest.clearConsole();
+
+            var config = {tagName: "div", classes: "logRow"};
+            FBTest.waitForDisplayedElement("console", config, function(row)
+            {
+                var expected = "message origin=http://legoas, data=test » Window iframe.html";
+                FBTest.compare(expected, row.textContent, "The proper message must be displayed.");
+                FBTest.testDone("issue5135.DONE");
+            });
+
+            // Execute test implemented on the test page.
+            FBTest.click(win.document.getElementById("testButton"));
+        });
+    });
+}
