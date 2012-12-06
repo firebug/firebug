063b8bb5a0a12a07cac153d10e1af6259ea40e5f
Jan Odvarko <odvarko@gmail.com>
2012-12-06 09:18:50 +0100

    FBTest: another trace improvements for cookiesPanel test

--- tests/content/cookies/general/cookiesPanel.js
+++ tests/content/cookies/general/cookiesPanel.js
@@ -1,34 +1,47 @@
 function runTest()
 {
+    var prevValue = FBTrace.DBG_TESTCASE;
+    FBTrace.DBG_TESTCASE = true;
+
+    var prevErrors = FBTrace.DBG_ERRORS;
+    FBTrace.DBG_ERRORS = true;
+
+    var prevSysout = FBTest.sysout;
+    FBTest.sysout = function(msg)
+    {
+        FBTestApp.TestRunner.appendResult(new FBTestApp.TestResult(window, true, "progress: "+msg));
+        FBTest.resetTimeout();
+    }
+
+    var prevTrace = FBTrace.sysout;
+    FBTrace.sysout = function(msg)
+    {
+        FBTestApp.TestRunner.appendResult(new FBTestApp.TestResult(window, true, "progress: "+msg));
+    }
+
     FBTest.sysout("cookiesPanel.START");
 
     FBTest.openNewTab(basePath + "cookies/general/cookiesPanel.html", function(win)
     {
-        FBTest.openFirebug(true);
-        FBTestFireCookie.enableCookiePanel(function(win) 
+        FBTest.openFirebug();
+        FBTestFireCookie.enableCookiePanel(function(win)
         {
             FBTest.sysout("cookiesPanel; Check existence of the Cookies panel.");
+            FBTest.progress("cookiesPanel; is Firebug open: " + FBTest.isFirebugOpen());
+            FBTest.progress("cookiesPanel; is Firebug active: " + FBTest.isFirebugActive());
+            FBTest.progress("Current context " + FW.Firebug.currentContext);
+            FBTest.progress("Cookies panel " + FBTest.getPanel("cookies"));
 
             // Make sure the Cookie panel's UI is there.
             var panel = FBTest.selectPanel("cookies");
-
-            if (!panel)
-            {
-                var context = FW.Firebug.currentContext;
-                FBTest.progress("Current context " + context.getName());
-                if (context)
-                {
-                    var names = [];
-                    var panelTypes = FW.Firebug.getMainPanelTypes(context);
-                    for (var i=0; i<panelTypes.length; i++)
-                        names.push(FW.Firebug.getPanelTitle(panelTypes[i]));
-                    FBTest.progress("Panels " + names.join(","));
-                }
-            }
-            else
-            {
+            if (panel)
                 FBTest.ok(panel.panelNode, "Cookies panel must be initialized.");
-            }
+
+            FBTrace.DBG_TESTCASE = prevValue;
+            FBTrace.DBG_ERRORS = prevErrors;
+
+            FBTest.sysout = prevSysout;
+            FBTrace.sysout = prevTrace;
 
             // Finish test
             FBTest.testDone("cookiesPanel.DONE");
