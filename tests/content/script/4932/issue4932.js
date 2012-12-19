244d97ef40b9f3cea9b96692cc3af7ada88918c2
Jan Odvarko <odvarko@gmail.com>
2012-12-19 12:17:26 +0100

    FBTest: first real try to fix script/4632 test

--- tests/content/script/4932/issue4932.js
+++ tests/content/script/4932/issue4932.js
@@ -11,24 +11,23 @@ function runTest()
 
             var config = {
                 tagName: "span",
-                classes: "sourceRowText"
+                classes: "sourceRowText",
+                counter: 3
             };
-
-            var panelNode = FBTest.getPanel("script").panelNode;
-            var nodes = panelNode.getElementsByClassName(config.classes);
-            FBTest.progress("Nodes: " + nodes.length);
 
             FBTest.waitForDisplayedElement("script", config, function(row)
             {
                 var panelNode = FBTest.getPanel("script").panelNode;
                 var nodes = panelNode.getElementsByClassName(config.classes);
 
-                for (var i=0; i<nodes.length; i++)
-                    FBTest.progress("n" + i + ": " + nodes[i].textContent);
-
-                var expected = /function funcTest\(\) \{\}\s*/;
-                FBTest.compare(expected, row.textContent,
-                    "The script panel must show expected source: " + row.textContent);
+                var length = nodes.length;
+                if (FBTest.compare(3, length, "There must be three nodes: " + length))
+                {
+                    var row = nodes[1];
+                    var expected = /function funcTest\(\) \{\}\s*/;
+                    FBTest.compare(expected, row.textContent,
+                        "The script panel must show expected source: " + row.textContent);
+                }
 
                 FBTest.testDone("issue4932.DONE");
             });
