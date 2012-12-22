be49148124b4a35928efa816d9b9012c9f08c09c
Jan Odvarko <odvarko@gmail.com>
2012-12-22 18:18:26 +0100

    FBTest: avoid exception

--- tests/FBTest/content/FBTestFirebug.js
+++ tests/FBTest/content/FBTestFirebug.js
@@ -414,6 +414,10 @@ this.synthesizeMouse = function(node, offsetX, offsetY, event, win)
 
     // Use the first client rect for clicking (e.g. SPAN can have more).
     var rect = rectCollection[0]; //node.getBoundingClientRect();
+
+    if (!FBTest.ok(rect, "Can't synthesize mouse event"))
+        return;
+
     var frameOffset = getFrameOffset(node);
 
     FBTest.sysout("frameOffset " + frameOffset);
