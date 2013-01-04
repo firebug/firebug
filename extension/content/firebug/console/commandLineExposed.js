firebug/firebug

commit 3118f395a2e7d9845e663b1b6071a4b95be74c7f

Initialized variables before using them and removed unused variables

extension/content/firebug/console/commandLineExposed.js
@@ -185,9 +185,9 @@ function createFirebugCommandLine(context, win)
     function evaluate(expr, origExpr)
     {
         var result;
+        var line = Components.stack.lineNumber;
         try
         {
-            var line = Components.stack.lineNumber;
             result = contentView.eval(expr);
 
             // See Issue 5221
@@ -198,8 +198,9 @@ function createFirebugCommandLine(context, win)
         {
             // change source and line number of exeptions from commandline code
             // create new error since properties of nsIXPCException are not modifiable
-            var shouldModify = false, isXPCException = false, dropFrames = false;
-            var fileName = exc.filename || exc.fileName, lineNumber;
+            var shouldModify = false, isXPCException = false;
+            var fileName = exc.filename || exc.fileName;
+            var lineNumber = 0;
             if (fileName.lastIndexOf("chrome:", 0) === 0)
             {
                 if (fileName === Components.stack.filename)
@@ -211,7 +212,6 @@ function createFirebugCommandLine(context, win)
                 }
                 else if (exc._dropFrames)
                 {
-                    dropFrames = true;
                     lineNumber = findLineNumberInExceptionStack(exc.stack);
                     shouldModify = (lineNumber !== null);
                 }
@@ -265,7 +265,7 @@ function createFirebugCommandLine(context, win)
                 eventID + " with " + objs.length + " user objects", commandLine.userObjects);
         }
 
-        var result;
+        var result = null;
         if (Dom.getMappedData(contentView.document, "firebug-retValueType") === "array")
             result = [];
 

extension/build.js
@@ -414,7 +414,7 @@ function clean()
 function zip(filename, directory, callback)
 {
     // Create final XPI package.
-    var zip;
+    var zip = null;
     if (os.platform() === "win32")
     {
         var params = "a -tzip " + filename + " " + directory + "/*";
@@ -426,10 +426,17 @@ function zip(filename, directory, callback)
         //zip = spawn("zip", [ "-r", __dirname + "/" + xpiFileName, release ]);
     }
 
-    zip.on("exit", function()
+    if (zip)
+    {
+        zip.on("exit", function()
+        {
+            callback();
+        });
+    }
+    else
     {
         callback();
-    });
+    }
 }
 
 // ********************************************************************************************* //

extension/content/firebug/accessible/a11y.js
@@ -466,7 +466,7 @@ Firebug.A11yModel = Obj.extend(Firebug.Module,
         var isButton = target.nodeName.search(/(xul:)?((toolbar)?button)|(checkbox)/) != -1;
         var isDropDownMenu = isButton && (target.getAttribute("type") == "menu" ||
             target.id == "fbLocationList");
-        var siblingTab, forward, toolbar, buttons;
+        var siblingTab, forward, toolbar;
         var keyCode = event.keyCode || (event.type == "keypress" ? event.charCode : null);
         if (keyCode == KeyEvent.DOM_VK_TAB)
             this.ensurePanelTabStops(); //TODO: need a better solution to prevent loss of panel tabstop
@@ -1176,10 +1176,10 @@ Firebug.A11yModel = Obj.extend(Firebug.Module,
             case KeyEvent.DOM_VK_RETURN:
             case KeyEvent.DOM_VK_SPACE:
                 var isEnter = (keyCode == KeyEvent.DOM_VK_RETURN);
-                var nodeLabels = null;
+                var nodeLabels = [];
                 if (isEnter)
                 {
-                    var nodeLabels = target.getElementsByClassName("nodeName");
+                    nodeLabels = target.getElementsByClassName("nodeName");
                     if (nodeLabels.length > 0)
                     {
                         Firebug.Editor.startEditing(nodeLabels[0]);
@@ -1684,7 +1684,6 @@ Firebug.A11yModel = Obj.extend(Firebug.Module,
             return;
         }
         var matchFeedback = "";
-        var matchType = "";
         var selector;
         if (Css.hasClass(matchRow, "cssSelector"))
         {
@@ -1807,7 +1806,6 @@ Firebug.A11yModel = Obj.extend(Firebug.Module,
         if (!Css.hasClass(target, "focusGroup"))
             return;
 
-        var panel = Firebug.getElementPanel(target);
         switch (keyCode)
         {
             case KeyEvent.DOM_VK_LEFT:
@@ -1872,7 +1870,7 @@ Firebug.A11yModel = Obj.extend(Firebug.Module,
         switch (panelA11y.type)
         {
             case "html":
-                var tagName= nodeName = null;
+                var nodeName = null;
                 var setSize = posInSet = 0; var setElems;
                 var label = Locale.$STR("a11y.labels.inline editor") + ": ";
                 if (Css.hasClass(target, "nodeName") || Css.hasClass(target, "nodeValue"))
@@ -2056,7 +2054,6 @@ Firebug.A11yModel = Obj.extend(Firebug.Module,
         var lineNo = parseInt(lineNode.getElementsByClassName("sourceLine").item(0).textContent);
         box.a11yCaretLine = lineNo;
         box.a11yCaretOffset = caretDetails[1];
-        var newLineNo = 1;
         var linesToScroll = 0;
         var goUp;
         switch (keyCode)
@@ -2134,7 +2131,7 @@ Firebug.A11yModel = Obj.extend(Firebug.Module,
                 liveString += "Line " + lineNo;
                 if (lineNode.getAttribute("breakpoint") == "true")
                 {
-                    var breakpointStr;
+                    var breakpointStr = "";
                     if (lineNode.getAttribute("disabledbreakpoint") == "true")
                         breakpointStr = "a11y.updates.has disabled breakpoint";
                     if (lineNode.getAttribute("condition") == "true")
@@ -2423,8 +2420,6 @@ Firebug.A11yModel = Obj.extend(Firebug.Module,
         if (!panelA11y || !rows)
             return;
 
-        var setSize;
-        var posInset;
         var setSize = rows.length;
         var posInset = 0;
         for (var i = 0; i < rows.length; i++)

extension/content/firebug/bti/inProcess/browser.js
@@ -242,10 +242,8 @@ Browser.prototype.closeContext = function(context, userCommands)
 
         if (shouldDispatch)
         {
-            var userCommands;
-
             // TODO remove
-            Events.dispatch(TabWatcher.fbListeners, "unwatchBrowser", [browser, userCommands]);
+            Events.dispatch(TabWatcher.fbListeners, "unwatchBrowser", [browser, null]);
             return true;
         }
         return false;
@@ -688,8 +686,6 @@ Browser.EventListener = {
 };
 
 // ********************************************************************************************* //
-
-var clearContextTimeout = 0;
 
 var TabWatchListener =
 {

extension/content/firebug/chrome/chrome.js
@@ -70,12 +70,6 @@ createFirebugChrome: function(win)
 
     var panelSplitter, sidePanelDeck, panelBar1, panelBar2;
 
-    var disabledHead = null;
-    var disabledCaption = null;
-    var enableSiteLink = null;
-    var enableSystemPagesLink = null;
-    var enableAlwaysLink = null;
-
 var FirebugChrome =
 {
     // TODO: remove this property, add getters for location, title, focusedElement, setter popup
@@ -776,7 +770,7 @@ var FirebugChrome =
 
     getSelectedPanelURL: function()
     {
-        var location;
+        var location = null;
         if (Firebug.currentContext)
         {
             var panel = Firebug.chrome.getSelectedPanel();

extension/content/firebug/chrome/firefox.js
@@ -7,7 +7,6 @@ define([], function() {
 
 var Ci = Components.interfaces;
 var Cc = Components.classes;
-var Cu = Components.utils;
 var wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
 
 // ********************************************************************************************* //

extension/content/firebug/chrome/tabWatcher.js
@@ -655,7 +655,7 @@ Firebug.TabWatcher = Obj.extend(new Firebug.Listener(),
             }
 
             Events.dispatch(this.fbListeners, "destroyContext",
-                [null, (browser?browser.persistedState:null), browser]);
+                [null, (browser ? browser.persistedState : null), browser]);
             return;
         }
 
@@ -670,9 +670,11 @@ Firebug.TabWatcher = Obj.extend(new Firebug.Listener(),
         Events.dispatch(this.fbListeners, "destroyContext", [context, persistedState, context.browser]);
 
         if (FBTrace.DBG_WINDOWS || FBTrace.DBG_ACTIVATION)
+        {
             FBTrace.sysout("-> tabWatcher.unwatchContext *** DESTROY *** context " + context.uid +
                 " for: " + (context.window && !context.window.closed?context.window.location :
                 "no window or closed ") + " aborted: " + context.aborted);
+        }
 
         context.destroy(persistedState);
 
@@ -687,7 +689,7 @@ Firebug.TabWatcher = Obj.extend(new Firebug.Listener(),
         if (!currentBrowser.showFirebug)
         {
             // context is null if we don't want to debug this browser
-            Events.dispatch(this.fbListeners, "showContext", [browser, null]);
+            Events.dispatch(this.fbListeners, "showContext", [currentBrowser, null]);
         }
     },
 

extension/content/firebug/chrome/tableRep.js
@@ -71,6 +71,7 @@ FirebugReps.Table = domplate(Firebug.Rep,
             return [row];
 
         var cols = [];
+        var value = null;
         for (var i=0; i<this.columns.length; i++)
         {
             var prop = this.columns[i].property;
@@ -88,7 +89,7 @@ FirebugReps.Table = domplate(Firebug.Rep,
             {
                 var props = (typeof(prop) == "string") ? prop.split(".") : [prop];
 
-                var value = row;
+                value = row;
                 for (var p in props)
                     value = (value && value[props[p]]) || undefined;
             }
@@ -212,11 +213,11 @@ FirebugReps.Table = domplate(Firebug.Rep,
         {
             this.columns = columns;
 
-            var object = object || {};
-            object.data = data;
-            object.columns = columns;
+            var obj = object || {};
+            obj.data = data;
+            obj.columns = columns;
 
-            var row = Firebug.Console.log(object, context, "table", this, true);
+            var row = Firebug.Console.log(obj, context, "table", this, true);
 
             // Set vertical height for scroll bar.
             var tBody = row.querySelector(".dataTableTbody");
@@ -270,7 +271,7 @@ FirebugReps.Table = domplate(Firebug.Rep,
     getHeaderColumns: function(data)
     {
         // Get the first row in the object.
-        var firstRow;
+        var firstRow = null;
         for (var p in data)
         {
             firstRow = data[p];
@@ -306,8 +307,6 @@ FirebugReps.Table = domplate(Firebug.Rep,
      */
     domFilter: function(object, name)
     {
-        var domMembers = Dom.getDOMMembers(object, name);
-
         if (typeof(object) == "function")
         {
             if (Dom.isDOMMember(object, name) && !Firebug.showDOMFuncs)

commit 2f6ff47aebc21e1343265d98aa573ba5770ef5cd

Fix calculation of line numbers of errors in evaluated expressions
Broken in 3118f39.

extension/content/firebug/console/commandLineExposed.js
@@ -185,10 +185,14 @@ function createFirebugCommandLine(context, win)
     function evaluate(expr, origExpr)
     {
         var result;
-        var line = Components.stack.lineNumber;
+        var baseLine;
         try
         {
-            result = contentView.eval(expr);
+            // Errors thrown from within the expression of the eval call will
+            // have a line number equal to (line of eval, 1-based) + (line in
+            // expression, 0-based) - keep track of the former term so we can
+            // correct it later.
+            baseLine = Components.stack.lineNumber; result = contentView.eval(expr);
 
             // See Issue 5221
             //var result = FirebugEvaluate(expr, contentView);
@@ -223,7 +227,7 @@ function createFirebugCommandLine(context, win)
                 result.stack = null;
                 result.source = expr;
                 result.message = exc.message;
-                result.lineNumber = lineNumber - line;
+                result.lineNumber = lineNumber - baseLine + 1;
 
                 // Lie and show the pre-transformed expression instead.
                 result.fileName = "data:," + encodeURIComponent(origExpr);