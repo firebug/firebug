66ec1aed655324bd57cc3aeeb736161d1ca90f5c
Jan Odvarko <odvarko@gmail.com>
2013-01-24 15:02:28 +0100

    Improve CMD tip example (issue 6200)

--- extension/content/firebug/console/commandLineHelp.js
+++ extension/content/firebug/console/commandLineHelp.js
@@ -261,7 +261,7 @@ var CommandLineHelp = domplate(
         });
 
         tips.push({
-            example: "object.%closureVarName",
+            example: "myObject.%closureVarName",
             text: "console.cmd.tip.closures"
         });
 
