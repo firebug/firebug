f3a3d88b85f9f2cc651603f2a1d665904aacd691
Sebastian Zartner <sebastianzartner@gmail.com>
2013-02-21 22:54:31 +0100

    FBTest for issue 6283 (Console and DOM panel fail on interpreting @page) http://code.google.com/p/fbug/issues/detail?id=6283

---
+++ tests/content/dom/6283/issue6283.js
@@ -0,0 +1,61 @@
+function runTest()
+{
+    FBTest.sysout("issue6283.START");
+
+    FBTest.openNewTab(basePath + "dom/6283/issue6283.html", function(win)
+    {
+        FBTest.openFirebug();
+        var panel = FBTest.selectPanel("stylesheet");
+
+        if (FBTest.selectPanelLocationByName(panel, "issue6283.html"))
+        {
+            FBTest.executeContextMenuCommand(FW.Firebug.chrome.$("fbLocationList"),
+                "InspectIndomPanel", function()
+            {
+                // xxxHonza, xxxsz: hack that fixes this text on Mac. The panel can 
+                // be selected asynchronously.
+                setTimeout(function() {
+                    onInspect();
+                }, 1000);
+            });
+        }
+        else
+        {
+            FBTest.testDone("issue6283.DONE");
+        }
+    });
+}
+
+function onInspect()
+{
+    var panel = FBTest.getSelectedPanel();
+    if (FBTest.compare("dom", panel.name, "DOM panel must be selected now"))
+    {
+        var props = panel.panelNode.getElementsByClassName("memberLabel");
+        var prop;
+
+        for (var i=0; i<props.length; i++)
+        {
+            if (props[i].textContent == "cssRules")
+            {
+                prop = props[i];
+                break;
+            }
+        }
+
+        var config = {tagName: "tr", classes: "memberRow ordinalRow"};
+        FBTest.waitForDisplayedElement("dom", config, function(row)
+        {
+            var cssRulesLabel = panel.panelNode.
+                querySelectorAll(".memberRow.ordinalRow[level=\"1\"] .memberLabel").item(0);
+            var cssRuleValue = FW.FBL.getAncestorByClass(cssRulesLabel, "memberRow").
+                getElementsByClassName("memberValueCell").item(0);
+
+            FBTest.compare("CSSPageRule", cssRuleValue.textContent.trim(), "Rule must be displayed as CSSPageRule");
+
+            FBTest.testDone("issue6283.DONE");
+        });
+
+        FBTest.click(prop);
+    }
+}
