bb3fdd6e7d2ec5bc9012dda90f9e60da055c0385
Jan Odvarko <odvarko@gmail.com>
2013-02-22 15:28:45 +0100

    FBTest for issue 5654 Style side panel doesn't display CSS rules after reload

---
+++ tests/content/css/style/5654/issue5654.js
@@ -0,0 +1,37 @@
+function runTest()
+{
+    FBTest.sysout("issue5654.START");
+    FBTest.openNewTab(basePath + "css/style/5654/issue5654.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.selectPanel("html");
+        FBTest.selectSidePanel("css");
+
+        // Catch the first page load and Style panel update.
+        waitForCssRules(function()
+        {
+            // Reload the page.
+            FBTest.reload(function()
+            {
+                // Catch the second style update
+                waitForCssRules(function()
+                {
+                    FBTest.testDone("issue5654.DONE");
+                });
+            });
+        });
+    });
+}
+
+function waitForCssRules(callback)
+{
+    var config = {tagName: "div", classes: "cssElementRuleContainer"};
+    FBTest.waitForDisplayedElement("css", config, function(row)
+    {
+        var panel = FBTest.selectSidePanel("css");
+        var nodes = panel.panelNode.querySelectorAll(".cssElementRuleContainer .cssRule");
+        FBTest.ok(nodes.length > 0, "There must be some styles: " + nodes.length);
+
+        callback();
+    });
+}
