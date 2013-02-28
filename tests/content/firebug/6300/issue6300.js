2880cd694855f552d81a885f8f97284413427316
Jan Odvarko <odvarko@gmail.com>
2013-02-28 18:16:10 +0100

    Issue 6300: 	Create FBTest that covers panel switching

---
+++ tests/content/firebug/6300/issue6300.js
@@ -0,0 +1,117 @@
+function runTest()
+{
+    var consoleButtons = [
+        "fbBreakOnNextButton",
+        "fbConsoleClear",
+        "fbConsolePersist",
+        "fbToggleProfiling",
+        "fbConsoleFilter-all",
+        "fbConsoleFilter-error",
+        "fbConsoleFilter-warning",
+        "fbConsoleFilter-info",
+        "fbConsoleFilter-debug",
+        "fbConsoleFilter-cookies",
+    ];
+
+    var htmlButtons = [
+        "fbBreakOnNextButton",
+        "fbToggleHTMLEditing",
+        "fbPanelStatus",
+    ];
+
+    var cssButtons = [
+        "fbToggleCSSEditing",
+        "fbLocationList",
+    ];
+
+    var scriptButtons = [
+        "fbBreakOnNextButton",
+        "fbScriptFilterMenu",
+        "fbLocationList",
+    ];
+
+    var domButtons = [
+        "fbPanelStatus",
+    ];
+
+    var netButtons = [
+        "fbBreakOnNextButton",
+        "fbNetClear",
+        "fbNetPersist",
+        "fbNetFilter-all",
+        "fbNetFilter-html",
+        "fbNetFilter-css",
+        "fbNetFilter-js",
+        "fbNetFilter-xhr",
+        "fbNetFilter-image",
+        "fbNetFilter-plugin",
+        "fbNetFilter-media",
+        "fbNetFilter-font",
+    ];
+
+    var cookiesButtons = [
+        "fbBreakOnNextButton",
+        "fcCookiesMenu",
+        "fcFilterMenu",
+        "fcPerm",
+    ];
+
+    FBTest.sysout("issue6300.START");
+    FBTest.openNewTab(basePath + "firebug/6300/issue6300.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableAllPanels();
+
+        FBTest.reload(function()
+        {
+            var tasks = new FBTest.TaskList();
+
+            // Iterate from the first panel
+            tasks.push(verifyButtonVisibility, "console", consoleButtons);
+            tasks.push(verifyButtonVisibility, "html", htmlButtons);
+            tasks.push(verifyButtonVisibility, "stylesheet", cssButtons);
+            tasks.push(verifyButtonVisibility, "script", scriptButtons);
+            tasks.push(verifyButtonVisibility, "dom", domButtons);
+            tasks.push(verifyButtonVisibility, "net", netButtons);
+            tasks.push(verifyButtonVisibility, "cookies", cookiesButtons);
+
+            // ... and back
+            tasks.push(verifyButtonVisibility, "cookies", cookiesButtons);
+            tasks.push(verifyButtonVisibility, "net", netButtons);
+            tasks.push(verifyButtonVisibility, "dom", domButtons);
+            tasks.push(verifyButtonVisibility, "script", scriptButtons);
+            tasks.push(verifyButtonVisibility, "stylesheet", cssButtons);
+            tasks.push(verifyButtonVisibility, "html", htmlButtons);
+            tasks.push(verifyButtonVisibility, "console", consoleButtons);
+
+            tasks.run(function()
+            {
+                FBTest.testDone("issue6300.DONE");
+            });
+        });
+    });
+}
+
+function verifyButtonVisibility(callback, panelName, buttons)
+{
+    FBTest.selectPanel(panelName);
+
+    for (var i=0; i<buttons.length; i++)
+    {
+        var buttonId = buttons[i];
+        var button = FW.Firebug.chrome.$(buttonId);
+
+        if (!FBTest.ok(button, "The button must be there: " + buttonId + " (" + panelName + ")"))
+            continue;
+
+        FBTest.ok(isVisible(button), "The button must be visible: " + buttonId +
+            " (" + panelName + ")");
+    }
+
+    callback();
+}
+
+function isVisible(elt)
+{
+    return (!elt.hidden && !elt.collapsed);
+}
