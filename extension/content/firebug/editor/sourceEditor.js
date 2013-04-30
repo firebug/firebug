7e7e724c303bdb90e9d37d11b44876de65bab324
Jan Odvarko <odvarko@gmail.com>
2013-04-30 16:17:07 +0200

    Properly include CM Files and display default (fixed) JS text

--- extension/content/firebug/editor/sourceEditor.js
+++ extension/content/firebug/editor/sourceEditor.js
@@ -3,12 +3,15 @@
 define([
     "firebug/firebug",
     "firebug/lib/http",
-    "firebug/editor/codemirror/codemirror",
+    "firebug/lib/dom",
 ],
-function (Firebug, Http, CodeMirror) {
+function (Firebug, Http, Dom) {
 
 // ********************************************************************************************* //
 // Constants
+
+var codeMirrorSrc = "chrome://firebug/content/editor/codemirror/codemirror.js";
+var jsMode = "chrome://firebug/content/editor/codemirror/mode/javascript.js";
 
 // xxxHonza: just temporary default text
 var defaultText = Http.getResource("chrome://firebug/content/net/netPanel.js");
@@ -24,19 +27,34 @@ function SourceEditor()
 
 SourceEditor.prototype =
 {
-    init: function (parentNode, config, callBcak)
+    init: function (parentNode, config, callback)
     {
-        this.editorObject = CodeMirror(function(elt)
+        var doc = parentNode.ownerDocument;
+
+        // Append CM scripts into the panel.html
+        Dom.addScript(doc, "cm", Http.getResource(codeMirrorSrc));
+        Dom.addScript(doc, "cm-js", Http.getResource(jsMode));
+
+        var self = this;
+        this.editorObject = doc.defaultView.CodeMirror(function(elt)
         {
             parentNode.appendChild(elt);
-            this.view = elt;
+            self.view = elt;
         },
         {
+            readOnly: true,
             mode: "javascript",
             value: defaultText,
             lineNumbers: true,
-            gutters: ["CodeMirror-lineNumbers"]
+            // xxxHonza: why this is here?
+            //gutters: ["CodeMirror-lineNumbers"],
+            theme: "firebug"
         });
+    },
+
+    setText: function(text)
+    {
+        // TODO
     }
 };
 
