352165ee288fd9cafccdabfb860c0fe11a7a4562
Sebastian Zartner <sebastianzartner@gmail.com>
2013-05-15 13:45:56 +0200

    Improved Obj.getObjHash() method to be able to distinguish between different nodes and object logs

--- extension/content/firebug/lib/object.js
+++ extension/content/firebug/lib/object.js
@@ -160,23 +160,31 @@ Obj.getObjHash = function(obj)
 {
     function getPropString(obj, level)
     {
+        level = level || 1;
         if (level > 3 || typeof obj != "object")
-            return "";
+            return typeof obj;
 
-        var str = "";
         try
         {
-            for (var prop in obj)
-            {
-                str += prop +
-                    (typeof obj == "object" ? getPropString(obj[prop], level + 1) : obj[prop]);
-            }
+            var props = Object.getOwnPropertyNames(obj);
         }
         catch(e)
         {
+            return typeof obj;
         }
 
-        return str;
+        var propString = "";
+        props.forEach(function(propName) {
+            propString += propName + ":" + typeof obj[propName] + ":" +
+                (typeof obj[propName] == "object" ?
+                    getPropString(obj[propName], level + 1) : obj[propName]);
+        });
+
+        var parent = Object.getPrototypeOf(obj);
+        if (parent)
+            propString += getPropString(parent);
+
+        return propString;
     }
 
     function toHexString(charCode)
@@ -184,28 +192,33 @@ Obj.getObjHash = function(obj)
         return ("0" + charCode.toString(16)).slice(-2);
     }
 
-    var str = "";
     try
     {
-        str = JSON.stringify(obj);
+        var str = typeof obj + getPropString(obj);
+    
+        var converter = Xpcom.CCSV("@mozilla.org/intl/scriptableunicodeconverter",
+            "nsIScriptableUnicodeConverter");
+        converter.charset = "UTF-8";
+        var result = {};
+        var data = converter.convertToByteArray(str, result);
+        var ch = Xpcom.CCSV("@mozilla.org/security/hash;1", "nsICryptoHash");
+        ch.init(ch.MD5);
+        ch.update(data, data.length);
+        var hash = ch.finish(false);
+    
+        var hexHash = "";
+        for (var i=0, len = hash.length; i < len; ++i)
+            hexHash += toHexString(hash.charCodeAt(i));
     }
     catch(e)
     {
-        str = getPropString(obj, 1);
+        if (FBTrace.DBG_ERRORS)
+            FBTrace.sysout("Obj.getObjHash; Hash could not be created", e);
+
+        return this.getUniqueId();
     }
 
-    var converter = Xpcom.CCSV("@mozilla.org/intl/scriptableunicodeconverter",
-        "nsIScriptableUnicodeConverter");
-    converter.charset = "UTF-8";
-    var result = {};
-    var data = converter.convertToByteArray(str, result);
-    var ch = Xpcom.CCSV("@mozilla.org/security/hash;1", "nsICryptoHash");
-    ch.init(ch.MD5);
-    ch.update(data, data.length);
-    var hash = ch.finish(false);
-     
-    var s = [toHexString(hash.charCodeAt(i)) for (i in hash)].join("");
-    return s;
+    return hexHash;
 };
 
 Obj.getRandomInt = function(min, max)
