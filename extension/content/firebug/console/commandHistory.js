firebug/firebug

commit 0621b58c54f56501653ec897cbdc2d9a602bc4cf

Issue 6004 (Avoid setting innerHTML where applicable) Part 2 http://code.google.com/p/fbug/issues/detail?id=6004

extension/content/firebug/console/commandHistory.js
@@ -130,7 +130,7 @@ Firebug.CommandHistory = function()
 
             hbox.classList.add("commandHistoryItem");
             var shortExpr = Str.cropString(Str.stripNewLines(commands[i]), 50);
-            hbox.innerHTML = Str.escapeForTextNode(shortExpr);
+            hbox.textContent = Str.escapeForTextNode(shortExpr);
             hbox.value = i;
             commandsPopup.appendChild(hbox);
 

extension/content/firebug/css/cssPanel.js
@@ -2663,7 +2663,7 @@ CSSRuleEditor.prototype = domplate(SelectorEditor.prototype,
             FBTrace.sysout("CSSRuleEditor.saveEdit: '" + value + "'  '" + previousValue +
                 "'", target);
 
-        target.innerHTML = Str.escapeForCss(value);
+        target.textContent = Str.escapeForCss(value);
         if (value === previousValue)
             return;
 
@@ -2774,7 +2774,7 @@ CSSRuleEditor.prototype = domplate(SelectorEditor.prototype,
                 if (FBTrace.DBG_CSS || FBTrace.DBG_ERRORS)
                     FBTrace.sysout("CSS Insert Error: "+err, err);
 
-                target.innerHTML = Str.escapeForCss(previousValue);
+                target.textContent = Str.escapeForCss(previousValue);
                 // create dummy rule to be able to recover from error
                 var insertLoc = CSSModule.insertRule(styleSheet,
                     'selectorSavingError{}', ruleIndex);
@@ -3037,7 +3037,7 @@ function getOriginalStyleSheetCSS(sheet, context)
 {
     if (sheet.ownerNode instanceof window.HTMLStyleElement)
     {
-        return sheet.ownerNode.innerHTML;
+        return sheet.ownerNode.textContent;
     }
     else
     {

extension/content/firebug/firebug.js
@@ -2427,7 +2427,7 @@ Firebug.MeasureBox =
 
     measureText: function(value)
     {
-        this.measureBox.innerHTML = value ? Str.escapeForSourceLine(value) : "m";
+        this.measureBox.textContent = value ? Str.escapeForSourceLine(value) : "m";
         return {width: this.measureBox.offsetWidth, height: this.measureBox.offsetHeight-1};
     },
 
@@ -2435,9 +2435,9 @@ Firebug.MeasureBox =
     {
         value = value ? Str.escapeForTextNode(value) : "m";
         if (!Firebug.showTextNodesWithWhitespace)
-            value = value.replace(/\t/g,'mmmmmm').replace(/\ /g,'m');
+            value = value.replace(/\t/g, "mmmmmm").replace(/\ /g, "m");
 
-        this.measureBox.innerHTML = value;
+        this.measureBox.textContent = value;
         return {width: this.measureBox.offsetWidth, height: this.measureBox.offsetHeight-1};
     },
 

extension/content/firebug/html/htmlPanel.js
@@ -608,12 +608,12 @@ Firebug.HTMLPanel.prototype = Obj.extend(WalkingPanel,
 
             if (nodeAttr && nodeAttr.childNodes.length > 3)
             {
-                var attrValueBox = nodeAttr.querySelector("*> .nodeValue");
+                var attrValueBox = nodeAttr.getElementsByClassName("nodeValue")[0];
                 var attrValueText = attrValueBox.firstChild;
                 if (attrValueText)
                     attrValueText.nodeValue = attrValue;
                 else
-                    attrValueBox.innerHTML = Str.escapeForTextNode(attrValue);
+                    attrValueBox.textContent = Str.escapeForTextNode(attrValue);
 
                 this.highlightMutation(attrValueBox, objectNodeBox, "mutated");
             }
@@ -2360,14 +2360,14 @@ TextNodeEditor.prototype = domplate(Firebug.InlineEditor.prototype,
             return;
 
         value = Str.unescapeForTextNode(value || "");
-        target.innerHTML = Str.escapeForTextNode(value);
+        target.textContent = Str.escapeForTextNode(value);
 
         if (node instanceof window.Element)
         {
             if (Xml.isElementMathML(node) || Xml.isElementSVG(node))
                 node.textContent = value;
             else
-                node.innerHTML = value;
+                node.textContent = value;
         }
         else
         {
@@ -2406,7 +2406,7 @@ AttributeEditor.prototype = domplate(Firebug.InlineEditor.prototype,
             return;
 
         // XXXstr unescape value
-        target.innerHTML = Str.escapeForElementAttribute(value);
+        target.textContent = Str.escapeForElementAttribute(value);
 
         if (Css.hasClass(target, "nodeName"))
         {

extension/content/firebug/js/sourceBox.js
@@ -664,8 +664,10 @@ Firebug.SourceBoxPanel = Obj.extend(SourceBoxPanelBase,
                 context.highlightedRow = lineNode;
 
                 if (FBTrace.DBG_COMPILATION_UNITS)
+                {
                     FBTrace.sysout("jumpHighlightFactory on line "+lineNo+" lineNode:"+
-                        lineNode.innerHTML);
+                        lineNode.getElementsById("sourceRowText")[0].textContent);
+                }
             }
             else
             {

extension/content/firebug/net/netReps.js
@@ -1661,7 +1661,7 @@ Firebug.NetMonitor.NetInfoHeaders = domplate(Firebug.Rep, new Firebug.Listener()
         var tbody = netInfoBox.getElementsByClassName("netInfo" + rowName + "Body").item(0);
         var node = this.sourceTag.replace({}, tbody);
         var sourceNode = node.getElementsByClassName("source").item(0);
-        sourceNode.innerHTML = source;
+        sourceNode.textContent = source;
     },
 
     insertHeaderRows: function(netInfoBox, headers, rowName)

extension/content/firebug/cookies/cookieReps.js
@@ -476,7 +476,7 @@ CookieReps.CookieRow = domplate(CookieReps.Rep,
     {
         if (Events.isLeftClick(event))
         {
-            var domain = event.target.innerHTML;
+            var domain = event.target.textContent;
             if (domain)
             {
                 Events.cancelEvent(event);
@@ -1375,18 +1375,17 @@ function checkList(panel)
     if (!panel || !this.panelNode)
         return; 
 
-    var row = Dom.getElementByClass(this.panelNode, "cookieRow");
+    var row = this.panelNode.getElementsByClassName("cookieRow")[0];
     while (row)
     {
         var rep = row.repObject;
-        if ((rep.cookie.name != row.firstChild.firstChild.innerHTML) ||
-            (rep.cookie.path != row.childNodes[3].firstChild.innerHTML))
+        var displayedCookieName = row.getElementsByClassName("cookieNameLabel")[0].textContent;
+        var displayedCookiePath = row.getElementsByClassName("cookiePathLabel")[0].textContent;
+        if (rep.cookie.name != displayedCookieName || rep.cookie.path != displayedCookiePath)
         {
-            FBTrace("---> Check failed!");
-            FBTrace("--->" + rep.rawHost + ", " + rep.cookie.name + ", " +
-                rep.cookie.path);
-            FBTrace("    " + row.firstChild.firstChild.innerHTML + ", " +
-                row.childNodes[3].firstChild.innerHTML);
+            FBTrace.sysout("---> Check failed!");
+            FBTrace.sysout("--->" + rep.rawHost + ", " + rep.cookie.name + ", " + rep.cookie.path);
+            FBTrace.sysout("    " + displayedCookieName + ", " + displayedCookiePath);
         }
 
         row = row.nextSibling;

extension/content/firebug/css/cssModule.js
@@ -111,7 +111,7 @@ Firebug.CSSModule = Obj.extend(Firebug.Module, Firebug.EditorSelector,
             styleSheet.editStyleSheet = editStyleSheet;
         }
 
-        styleSheet.editStyleSheet.innerHTML = value;
+        styleSheet.editStyleSheet.textContent = value;
 
         if (FBTrace.DBG_CSS)
             FBTrace.sysout("css.saveEdit styleSheet.href:" + styleSheet.href +

commit 74e98b7aca2fe9a268b5e7f472cb6af69cdb7293

Fixed issues introduced in 0621b58c54 (according to Simon's comments)

extension/content/firebug/console/commandHistory.js
@@ -130,7 +130,7 @@ Firebug.CommandHistory = function()
 
             hbox.classList.add("commandHistoryItem");
             var shortExpr = Str.cropString(Str.stripNewLines(commands[i]), 50);
-            hbox.textContent = Str.escapeForTextNode(shortExpr);
+            hbox.textContent = shortExpr;
             hbox.value = i;
             commandsPopup.appendChild(hbox);
 

extension/content/firebug/css/cssPanel.js
@@ -2663,7 +2663,7 @@ CSSRuleEditor.prototype = domplate(SelectorEditor.prototype,
             FBTrace.sysout("CSSRuleEditor.saveEdit: '" + value + "'  '" + previousValue +
                 "'", target);
 
-        target.textContent = Str.escapeForCss(value);
+        target.textContent = value;
         if (value === previousValue)
             return;
 
@@ -2774,7 +2774,7 @@ CSSRuleEditor.prototype = domplate(SelectorEditor.prototype,
                 if (FBTrace.DBG_CSS || FBTrace.DBG_ERRORS)
                     FBTrace.sysout("CSS Insert Error: "+err, err);
 
-                target.textContent = Str.escapeForCss(previousValue);
+                target.textContent = previousValue;
                 // create dummy rule to be able to recover from error
                 var insertLoc = CSSModule.insertRule(styleSheet,
                     'selectorSavingError{}', ruleIndex);

extension/content/firebug/firebug.js
@@ -2427,13 +2427,14 @@ Firebug.MeasureBox =
 
     measureText: function(value)
     {
-        this.measureBox.textContent = value ? Str.escapeForSourceLine(value) : "m";
+        this.measureBox.textContent = value || "m";
         return {width: this.measureBox.offsetWidth, height: this.measureBox.offsetHeight-1};
     },
 
     measureInputText: function(value)
     {
-        value = value ? Str.escapeForTextNode(value) : "m";
+        if (!value)
+            value = "m";
         if (!Firebug.showTextNodesWithWhitespace)
             value = value.replace(/\t/g, "mmmmmm").replace(/\ /g, "m");
 

extension/content/firebug/html/htmlPanel.js
@@ -613,7 +613,7 @@ Firebug.HTMLPanel.prototype = Obj.extend(WalkingPanel,
                 if (attrValueText)
                     attrValueText.nodeValue = attrValue;
                 else
-                    attrValueBox.textContent = Str.escapeForTextNode(attrValue);
+                    attrValueBox.textContent = attrValue;
 
                 this.highlightMutation(attrValueBox, objectNodeBox, "mutated");
             }
@@ -2360,14 +2360,14 @@ TextNodeEditor.prototype = domplate(Firebug.InlineEditor.prototype,
             return;
 
         value = Str.unescapeForTextNode(value || "");
-        target.textContent = Str.escapeForTextNode(value);
+        target.textContent = value;
 
         if (node instanceof window.Element)
         {
             if (Xml.isElementMathML(node) || Xml.isElementSVG(node))
                 node.textContent = value;
             else
-                node.textContent = value;
+                node.innerHTML = value;
         }
         else
         {
@@ -2406,7 +2406,7 @@ AttributeEditor.prototype = domplate(Firebug.InlineEditor.prototype,
             return;
 
         // XXXstr unescape value
-        target.textContent = Str.escapeForElementAttribute(value);
+        target.textContent = value;
 
         if (Css.hasClass(target, "nodeName"))
         {

extension/content/firebug/js/sourceBox.js
@@ -666,7 +666,7 @@ Firebug.SourceBoxPanel = Obj.extend(SourceBoxPanelBase,
                 if (FBTrace.DBG_COMPILATION_UNITS)
                 {
                     FBTrace.sysout("jumpHighlightFactory on line "+lineNo+" lineNode:"+
-                        lineNode.getElementsById("sourceRowText")[0].textContent);
+                        lineNode.getElementsByClassName("sourceRowText")[0].textContent);
                 }
             }
             else

extension/content/firebug/net/netReps.js
@@ -1643,7 +1643,7 @@ Firebug.NetMonitor.NetInfoHeaders = domplate(Firebug.Rep, new Firebug.Listener()
         else
         {
             var source = requestHeaders ? file.requestHeadersText : file.responseHeadersText;
-            this.insertSource(netInfoBox, Str.escapeForTextNode(source), target.rowName);
+            this.insertSource(netInfoBox, source, target.rowName);
             target.textContent = Locale.$STR("net.headers.pretty print");
         }
 
@@ -1654,10 +1654,6 @@ Firebug.NetMonitor.NetInfoHeaders = domplate(Firebug.Rep, new Firebug.Listener()
 
     insertSource: function(netInfoBox, source, rowName)
     {
-        // This breaks copy to clipboard.
-        //if (source)
-        //    source = source.replace(/\r\n/gm, "<span style='color:lightgray'>\\r\\n</span>\r\n");
-
         var tbody = netInfoBox.getElementsByClassName("netInfo" + rowName + "Body").item(0);
         var node = this.sourceTag.replace({}, tbody);
         var sourceNode = node.getElementsByClassName("source").item(0);