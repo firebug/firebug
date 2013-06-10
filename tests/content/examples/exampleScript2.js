390b1a96e74318c43f68f5575ef0d2438add74bb
Jan Odvarko <odvarko@gmail.com>
2013-06-10 18:18:23 +0200

    FBTest: new FBTest example (set a breakpoint)

---
+++ tests/content/examples/exampleScript2.js
@@ -0,0 +1,28 @@
+function runTest()
+{
+    FBTest.sysout("exampleScript2.START");
+
+    FBTest.openNewTab(basePath + "examples/exampleScript2.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableScriptPanel(function(win)
+        {
+            var chrome = FW.Firebug.chrome;
+            var panel = chrome.selectPanel("script");
+
+            // Set breakpoint
+            var lineNo = 23;
+            FBTest.setBreakpoint(chrome, null, lineNo, null, function()
+            {
+                FBTest.waitForBreakInDebugger(chrome, lineNo, false, function(row)
+                {
+                    FBTest.clickContinueButton();
+                    FBTest.testDone("exampleScript2.DONE");
+                });
+
+                // Execute test script on the page.
+                FBTest.clickContentButton(win, "testButton");
+            });
+        });
+    });
+}
