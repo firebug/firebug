56694c250e419a91b25f788161fe746a4c5088d4
Jan Odvarko <odvarko@gmail.com>
2013-06-26 13:43:32 +0200

    FBTest for issue 6530

---
+++ tests/content/net/filter/6530/issue6530.js
@@ -0,0 +1,51 @@
+function runTest()
+{
+    FBTest.sysout("issue6530.START");
+
+    FBTest.openNewTab(basePath + "net/filter/6530/issue6530.html", function(win)
+    {
+        FBTest.enableNetPanel(function(win)
+        {
+            FBTest.clearCache();
+
+            var config = {
+                tagName: "tr",
+                classes: "netRow category-js hasHeaders loaded",
+            };
+
+            FBTest.waitForDisplayedElement("net", config, function(row)
+            {
+                var panel = FW.Firebug.chrome.selectPanel("net");
+
+                FBTest.clickToolbarButton(null, "fbNetFilter-js");
+
+                setTimeout(checkNetPanelUI, 300);
+            });
+
+            // Execute test on the test page.
+            FBTest.click(win.document.getElementById("testButton"));
+        });
+    });
+}
+
+// Make sure the Net panel's UI is properly filtered.
+function checkNetPanelUI()
+{
+    var panelNode = FBTest.getPanel("net").panelNode;
+
+    var netRows = panelNode.getElementsByClassName("netRow category-js hasHeaders loaded");
+    FBTest.compare(1, netRows.length, "There must be exactly one request displayed!");
+
+    // Each row can specify just one category.
+    for (var i=0; i<netRows.length; i++)
+    {
+        var row = netRows[i];
+        var file = FW.Firebug.getRepObject(row);
+        var m = row.className.match(/category-/gi);
+        FBTest.compare(1, m.length, "There must be just one file category specified for a request: " + 
+            file.href);
+    }
+
+    FW.Firebug.NetMonitor.onToggleFilter(FW.Firebug.currentContext, "all");
+    FBTest.testDone("issue6530.DONE");
+}
