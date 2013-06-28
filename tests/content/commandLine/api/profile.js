af0423d1a5326d6a277c7342e539bc9d1de49426
Simon Lindholm <simon.lindholm10@gmail.com>
2013-06-29 01:49:08 +0200

    Extend the FBTests for profile, console.profile to check for warnings about Script panel required

--- tests/content/commandLine/api/profile.js
+++ tests/content/commandLine/api/profile.js
@@ -4,18 +4,23 @@ function runTest()
     FBTest.openNewTab(basePath + "commandLine/api/profile.html", function(win)
     {
         FBTest.openFirebug();
-        FBTest.enableScriptPanel();
-        FBTest.enableConsolePanel(function(win)
-        {
-            var config = {tagName: "table", classes: "profileTable"};
-            FBTest.waitForDisplayedElement("console", config, function(row)
-            {
-                FBTest.testDone("commandline.profile.DONE");
-            });
+        FBTest.enableConsolePanel();
 
-            FBTest.executeCommand("profile()");
-            FBTest.click(win.document.getElementById("testButton"));
-            FBTest.executeCommand("profileEnd()");
-        });
+        var expectedWarning = FW.FBL.$STR("ProfilerRequiresTheScriptPanel");
+        FBTest.executeCommandAndVerify(function()
+        {
+            FBTest.enableScriptPanel(function(win)
+            {
+                var config = {tagName: "table", classes: "profileTable"};
+                FBTest.waitForDisplayedElement("console", config, function(row)
+                {
+                    FBTest.testDone("commandline.profile.DONE");
+                });
+
+                FBTest.executeCommand("profile()");
+                FBTest.click(win.document.getElementById("testButton"));
+                FBTest.executeCommand("profileEnd()");
+            });
+        }, "profile();", expectedWarning, "div", "logRow-warn", true);
     });
 }
