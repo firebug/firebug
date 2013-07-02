bbed0dcec8e15ce0e960bc3c819a5c25d7e42265
Sebastian Zartner <sebastianzartner@gmail.com>
2013-07-02 10:15:54 +0200

    Improved test case to cover node removals and content changes

--- tests/content/css/selectors/6540/issue6540.js
+++ tests/content/css/selectors/6540/issue6540.js
@@ -5,21 +5,52 @@ function runTest()
     FBTest.openNewTab(basePath + "css/selectors/6540/issue6540.html", function(win)
     {
         FBTest.openFirebug();
-        FBTest.selectPanel("selectors");
+        var panel = FBTest.selectSidePanel("selectors");
 
-        FBTest.addSelectorTrial(null, "div", function(objectLink) {
-            var config = {
-                tagName: "a",
-                classes: "objectLink-element"
-            };
+        FBTest.addSelectorTrial(null, "#test > div:not(:empty)", function(objectLink) {
+            var panelNode = panel.panelNode;
+            var divs = panelNode.getElementsByClassName("objectLink-element");
 
-            FBTest.waitForDisplayedElement("selectors", config, function(row)
+            if (FBTest.compare(1, divs.length, "There must be one <div> listed"))
+            {
+                clickAndVerify(win, "removeDIV", 0, "There must be no <div>s listed", function () {
+                    clickAndVerify(win, "addDIV", 1, "There must be one <div> listed",
+                        function ()
+                        {
+                            clickAndVerify(win, "changeText", 0, "There must be no <div>s listed",
+                                function () {
+                                    FBTest.testDone("issue6540.DONE");
+                                }
+                            );
+                        }
+                    );
+                });
+            }
+            else
             {
                 FBTest.testDone("issue6540.DONE");
-            });
-
-            var button = win.document.getElementById("addDIV");
-            FBTest.click(button);
+            }
         });
     });
+}
+
+function clickAndVerify(win, id, expected, msg, callback)
+{
+    var config = {
+        tagName: "a",
+        classes: "objectLink-element"
+    };
+
+    FBTest.waitForDisplayedElement("selectors", config, function(row)
+    {
+        var panelNode = FBTest.getSelectedSidePanel().panelNode;
+        var divs = panelNode.getElementsByClassName("objectLink-element");
+        if (FBTest.compare(expected, divs.length, msg))
+            callback();
+        else
+            FBTest.testDone("issue6540.DONE");
+    });
+
+    var button = win.document.getElementById(id);
+    FBTest.click(button);
 }
