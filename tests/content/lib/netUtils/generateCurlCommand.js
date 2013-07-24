31f4eb3b487c4d0b8615766ebc01f0f62210987d
Thomas Andersen <tan@enonic.com>
2013-07-24 13:49:59 +0200

    6616: Net Panel: Copy request as a cURL command - improve performance of generateCurlCommand tests

--- tests/content/lib/netUtils/generateCurlCommand.js
+++ tests/content/lib/netUtils/generateCurlCommand.js
@@ -1,10 +1,10 @@
 function runTest()
 {
-    FBTest.sysout("clipboard-text.START");
+    FBTest.sysout("generateCurlCommand.START");
 
     var HOST = FW.FBL.makeURI(basePath).host;
-    var REQUEST_URL = basePath + "net/copy-as-curl/server.php";
-    var REFERRER_URL = basePath + "net/copy-as-curl/clipboard-text-form.html";
+    var REQUEST_URL = basePath + "lib/netUtils/server.php";
+    var REFERRER_URL = basePath + "lib/netUtils/generateCurlCommand.html";
 
     var EXPECTED_GET_RESULT = "curl '" + REQUEST_URL + "?param1=12345&param2=Test%20&%20Test&param3=$&+,/:;=?@%20#%{}|\\^~[]%C2%B4%27' -H 'Host: " + HOST + "' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:22.0) Gecko/20100101 Firefox/22.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate' -H 'Referer: " + REFERRER_URL + "' -H 'Connection: keep-alive'";
     var EXPECTED_POST_RESULT = "curl '" + REQUEST_URL + "' -H 'Host: " + HOST + "' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:22.0) Gecko/20100101 Firefox/22.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Referer: " + REFERRER_URL + "' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' --data $'param1=12345&param2=Test & Test&param3=$&+,/:;=?@ #%{}|\\\\^~[]\\xb4\\''";
@@ -13,65 +13,50 @@ function runTest()
     var EXPECTED_DELETE_RESULT = "curl '" + REQUEST_URL + "' -X DELETE -H 'Host: " + HOST + "' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:22.0) Gecko/20100101 Firefox/22.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate' -H 'Referer: " + REFERRER_URL + "' -H 'Connection: keep-alive'";
 
     FBTest.setPref("net.curlAddCompressedArgument", false);
-    FBTest.openNewTab(basePath + "net/copy-as-curl/clipboard-text-form.html", function (win)
+    FBTest.openNewTab(basePath + "lib/netUtils/generateCurlCommand.html", function (win)
     {
         FBTest.enableNetPanel(function (win)
         {
             var tasks = new FBTest.TaskList();
-            tasks.push(submitAndVerify, "GET", EXPECTED_GET_RESULT, win);
-            tasks.push(submitAndVerify, "POST", EXPECTED_POST_RESULT, win);
-            tasks.push(submitAndVerify, "HEAD", EXPECTED_HEAD_RESULT, win);
-            tasks.push(submitAndVerify, "PUT", EXPECTED_PUT_RESULT, win);
-            tasks.push(submitAndVerify, "DELETE", EXPECTED_DELETE_RESULT, win);
+            tasks.push(submitAndVerify, "GET", EXPECTED_GET_RESULT, win, "Generated cURL command from a GET request result should be correct");
+            tasks.push(submitAndVerify, "POST", EXPECTED_POST_RESULT, win, "Generated cURL command from a POST request result should be correct");
+            tasks.push(submitAndVerify, "HEAD", EXPECTED_HEAD_RESULT, win, "Generated cURL command from a HEAD request result should be correct");
+            tasks.push(submitAndVerify, "PUT", EXPECTED_PUT_RESULT, win, "Generated cURL command from a PUT request result should be correct");
+            tasks.push(submitAndVerify, "DELETE", EXPECTED_DELETE_RESULT, win, "Generated cURL command from a DELETE request result should be correct");
 
             tasks.run(function ()
             {
                 FBTest.cleanUpTestTabs();
-                FBTest.testDone("clipboard-text.DONE");
+                FBTest.testDone("generateCurlCommand.DONE");
             });
         });
     });
 }
 
-function submitAndVerify(taskCallback, method, expectedResult, win)
+function submitAndVerify(taskCallback, method, expectedResult, win, whatToExpectText)
 {
     FBTest.progress("Test XHR " + method + " request");
 
     FBTest.selectPanel("net").clear();
-
-    var options =
+    onRequestDisplayed(function(netRow)
     {
-        tagName: "tr",
-        classes: "netRow category-xhr hasHeaders loaded"
-    };
+        var file = FW.Firebug.getRepObject(netRow);
+        var generatedCommand = FW.Firebug.NetMonitor.Utils.generateCurlCommand(file);
+        FBTest.compare(generatedCommand, expectedResult, whatToExpectText);
 
-    FBTest.waitForDisplayedElement("net", options, function (row)
-    {
-        executeAndVerify(expectedResult, "fbCopyAsCurl", row, taskCallback);
+        taskCallback();
     });
 
     win.wrappedJSObject.submitForm(method, true);
 }
 
-function executeAndVerify(expectedResult, commandId, netRow, taskCallback)
+function onRequestDisplayed(callback)
 {
-    // Open context menu on the specified target.
-    FBTest.executeContextMenuCommand(netRow, commandId, function ()
-    {
-        // Data can be copyied into the clipboard asynchronously,
-        // so wait till they are available.
-        FBTest.waitForClipboard(expectedResult, function (clipboardText)
-        {
-            function replaceUserAgentHeader(str)
-            {
-                var replaceWithStr = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:22.0) Gecko/20100101 Firefox/22.0";
-                return str.replace(/(-H 'User-Agent: ).+?(')/i, "$1" + replaceWithStr + "$2");
-            }
+    // Create listener for mutation events.
+    var doc = FBTest.getPanelDocument();
+    var recognizer = new MutationRecognizer(doc.defaultView, "tr",
+        {"class": "netRow category-xhr loaded"});
 
-            // Verify data in the clipboard
-            FBTest.compare(expectedResult, replaceUserAgentHeader(clipboardText), "Proper data must be in the clipboard. Current: " + clipboardText);
-
-            taskCallback();
-        });
-    });
+    // Wait for a XHR log to appear in the Net panel.
+    recognizer.onRecognizeAsync(callback);
 }
