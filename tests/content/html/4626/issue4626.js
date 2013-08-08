a838953bb8d277edf04b6c79fd004ff7e15c3fb9
Sebastian Zartner <sebastianzartner@gmail.com>
2013-08-09 01:41:47 +0200

    FBTest for issue 4626 (Display of doctype for frames) http://code.google.com/p/fbug/issues/detail?id=4626

---
+++ tests/content/html/4626/issue4626.js
@@ -0,0 +1,26 @@
+function runTest()
+{
+    FBTest.sysout("issue4626.START");
+
+    FBTest.openNewTab(basePath + "html/4626/issue4626.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.selectPanel("html");
+
+        FBTest.searchInHtmlPanel("testFrame", function (sel)
+        {
+            // Click on the element to make sure it's selected
+            var iframeNode = FW.FBL.getAncestorByClass(sel.anchorNode, "containerNodeBox open");
+            var docTypeNode = iframeNode.getElementsByClassName("docTypeNodeBox")[0];
+
+            if (FBTest.ok(docTypeNode, "Doctype must exist"))
+            {
+                FBTrace.sysout("docTypeNode", docTypeNode);
+                FBTest.compare("<!DOCTYPE html>", docTypeNode.textContent,
+                    "Doctype must be correct");
+            }
+
+            FBTest.testDone("issue4626.DONE");
+        });
+    });
+}
