d5b0a750492b6d8e84b458cd0ad812d7247ae14b
Sebastian Zartner <sebastianzartner@gmail.com>
2013-10-25 12:06:41 +0200

    Issue 4001 (Part 2): Added tooltip for error badge

--- extension/content/firebug/firefox/start-button/startButtonOverlay.js
+++ extension/content/firebug/firefox/start-button/startButtonOverlay.js
@@ -136,6 +136,7 @@ Firebug.StartButton = Obj.extend(Module,
                 var errorLabel = Firefox.getElementById("firebug-error-label");
                 errorBadge.setAttribute("showErrors", "true");
                 errorLabel.setAttribute("value", errorCount);
+                errorLabel.setAttribute("tooltiptext", Locale.$STRP("plural.startbutton.tip.errors", [errorCount]))
             }
         }
         else
