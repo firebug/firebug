8b397b86c107d06f543c2c4a65a8575d7bec254b
Jakob Kaltenbrunner <jakob.kaltenbrunner@gmx.at>
2013-11-01 22:24:36 +0100

    - fixed missing bracket

--- extension/content/firebug/debugger/script/scriptPanel.js
+++ extension/content/firebug/debugger/script/scriptPanel.js
@@ -1017,7 +1017,7 @@ ScriptPanel.prototype = Obj.extend(BasePanel,
         Trace.sysout("scriptPanel.toggleBreakpoint; " + line);
 
         var hasBreakpoint = BreakpointStore.hasBreakpoint(this.getCurrentURL(), line);
-        if (hasBreakpoint)
+        if (hasBreakpoint) {
             BreakpointStore.removeBreakpoint(this.getCurrentURL(), line);
             if (this.scriptView.editor.debugLocation == line) 
                 this.scriptView.setDebugLocation(line);
