f7e7fcf0bcf7699094772cd87011f1ddf560232b
Simon Lindholm <simon.lindholm10@gmail.com>
2013-11-10 13:01:09 +0100

    FBTest for issue 4934 (Auto-completion should work even in the Watch panel)
    https://code.google.com/p/fbug/issues/detail?id=4934

---
+++ tests/content/script/watch/4934/issue4934.js
@@ -0,0 +1,46 @@
+function runTest()
+{
+    FBTest.sysout("issue4934.START");
+
+    FBTest.openNewTab(basePath + "script/watch/4934/issue4934.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.selectPanel("script");
+
+        FBTest.enableScriptPanel(function(win)
+        {
+            FBTest.waitForBreakInDebugger(null, 17, false, function()
+            {
+                // Like FBTest.addWatchExpression
+
+                var chrome = FW.Firebug.chrome;
+
+                var watchPanel = FBTest.getPanel("watches", true);
+                FBTest.ok(watchPanel, "The watch panel must be there");
+                var panelNode = watchPanel.panelNode;
+                var watchNewRow = panelNode.querySelector(".watchEditBox");
+                FBTest.ok(watchNewRow, "The watch edit box must be there");
+                FBTest.mouseDown(watchNewRow);
+
+                var editor = panelNode.querySelector(".completionInput");
+                FBTest.ok(editor, "The editor must be there");
+
+                FBTest.sendString("argu", editor);
+                FBTest.sendKey("TAB", editor);
+                FBTest.compare("arguments", editor.value, "Must auto-complete 'argu' -> 'arguments'");
+
+                FBTest.sendString(".cal", editor);
+                FBTest.sendKey("TAB", editor);
+
+                FBTest.compare("arguments.callee", editor.value, "Must auto-complete 'arguments.cal' -> 'arguments.callee'");
+
+                // Resume debugger
+                FBTest.clickContinueButton(null);
+
+                FBTest.testDone("issue4934.DONE");
+            });
+
+            FBTest.click(win.document.getElementById("executeTest"));
+        });
+    });
+}
