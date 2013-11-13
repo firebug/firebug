f496f7d6d95869c7e8404a3761d781b77f7bff8b
Jan Odvarko <odvarko@gmail.com>
2013-11-13 16:36:19 +0100

    Do not derive from main panels

--- extension/content/firebug/dom/domSidePanel.js
+++ extension/content/firebug/dom/domSidePanel.js
@@ -3,10 +3,19 @@
 /*global FBTrace:true, XPCNativeWrapper:true, Window:true, define:true */
 
 define([
+    "firebug/firebug",
+    "firebug/lib/trace",
     "firebug/lib/object",
-    "firebug/dom/domPanel",
+    "firebug/dom/domBasePanel",
+    "firebug/dom/domPanelTree",
+    "firebug/dom/domProvider",
+    "firebug/dom/domMemberProvider",
+    "firebug/dom/toggleBranch",
 ],
-function(Obj, DOMPanel) {
+function(Firebug, FBTrace, Obj, DOMBasePanel, DomPanelTree, DomProvider, DOMMemberProvider,
+    ToggleBranch) {
+
+"use strict";
 
 // ********************************************************************************************* //
 // Constants
@@ -21,14 +30,29 @@ function DOMSidePanel()
 {
 }
 
-DOMSidePanel.prototype = Obj.extend(DOMPanel.prototype,
+DOMSidePanel.prototype = Obj.extend(DOMBasePanel.prototype,
 /** @lends DOMSidePanel */
 {
     name: "domSide",
     parentPanel: "html",
     order: 3,
     enableA11y: true,
-    deriveA11yFrom: "console"
+    deriveA11yFrom: "console",
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+    // Initialization
+
+    initialize: function()
+    {
+        // Content rendering
+        this.provider = new DomProvider(this);
+        this.tree = new DomPanelTree();
+        this.tree.provider = this.provider;
+        this.tree.memberProvider = new DOMMemberProvider(this.context);
+        this.toggles = new ToggleBranch.ToggleBranch();
+
+        DOMBasePanel.prototype.initialize.apply(this, arguments);
+    },
 });
 
 // ********************************************************************************************* //
