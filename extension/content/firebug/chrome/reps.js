147f0bc62d2ecb674c54cf8abd1e372e256c0d7e
Simon Lindholm <simon.lindholm10@gmail.com>
2014-02-07 19:50:00 +0100

    Avoid duplicate l10n for some strings

--- extension/content/firebug/chrome/reps.js
+++ extension/content/firebug/chrome/reps.js
@@ -1281,7 +1281,8 @@ FirebugReps.Element = domplate(Rep,
         {
             label: Locale.$STRF("html.Copy_Node", [type]),
             tooltiptext: Locale.$STRF("html.tip.Copy_Node", [type]),
-            command: Obj.bindFixed(this.copyHTML, this, elt)
+            command: Obj.bindFixed(this.copyHTML, this, elt),
+            nol10n: true
         });
 
         if (Xml.isElementHTMLOrXHTML(elt))
@@ -1310,6 +1311,7 @@ FirebugReps.Element = domplate(Rep,
             {
                 label: Locale.$STRF("html.menu.Paste", [type]),
                 tooltiptext: Locale.$STRF("html.tip.Paste", [type]),
+                nol10n: true,
                 disabled: !clipboardContent,
                 id: "fbPaste",
                 items: [
