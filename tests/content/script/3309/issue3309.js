faf3a2c5faba83468ee19db280d413cb42f5e358
Jan Odvarko <odvarko@gmail.com>
2014-02-11 08:27:28 +0100

    FBTest: fixing other tests in the Script group (related to issue 7166)

--- tests/content/script/3309/issue3309.js
+++ tests/content/script/3309/issue3309.js
@@ -6,26 +6,11 @@ function runTest()
 
     FBTest.openNewTab(basePath + "script/3309/issue3309.html", function(win)
     {
-        FBTest.openFirebug();
-
-        // Enable script panel, but don't reload.
-        var panelType = FW.Firebug.getPanelType("script");
-        FW.Firebug.PanelActivation.setPanelState(panelType, true);
-
-        var panelNode = FBTest.selectPanel("script").panelNode;
-
-        // Check the content
-        var header = panelNode.querySelector(".disabledPanelHead");
-        if (FBTest.ok(header, "The header must be there"))
-        {
-            var text = FW.FBL.$STR("script.warning.inactive_during_page_load");
-            FBTest.compare(text, header.textContent,
-                "The page must display expected text: " + text);
-        }
-
-        FBTest.reload(function(win)
+        FBTest.enableScriptPanel(function(win)
         {
             var panelNode = FBTest.selectPanel("script").panelNode;
+
+            // Check the content
             var header = panelNode.querySelector(".disabledPanelHead");
             if (FBTest.ok(header, "The header must be there"))
             {
@@ -34,7 +19,19 @@ function runTest()
                     "The page must display expected text: " + text);
             }
 
-            FBTest.testDone("issue3309.DONE");
+            FBTest.reload(function(win)
+            {
+                var panelNode = FBTest.selectPanel("script").panelNode;
+                var header = panelNode.querySelector(".disabledPanelHead");
+                if (FBTest.ok(header, "The header must be there"))
+                {
+                    var text = FW.FBL.$STR("script.warning.no_javascript");
+                    FBTest.compare(text, header.textContent,
+                        "The page must display expected text: " + text);
+                }
+
+                FBTest.testDone("issue3309.DONE");
+            });
         });
     });
 }
