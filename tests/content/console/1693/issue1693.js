a49a8b16d630f48401697b6e4cc9d52f831030d8
Sebastian Zartner <sebastianzartner@gmail.com>
2014-02-20 16:16:16 +0100

    Fixed FBTest console/1693/issue1693.js

--- tests/content/console/1693/issue1693.js
+++ tests/content/console/1693/issue1693.js
@@ -13,54 +13,58 @@ function runTest()
                 FBTest.resetTimeout(win);
             }, true);
 
-            win.document.addEventListener("data-complete", function() {
-                onDataComplete();
-            }, true);
+            win.document.addEventListener("data-complete", onDataComplete, true);
 
             FBTest.click(win.document.getElementById("testButton"));
         });
     });
 }
 
-function onDataComplete()
+function onDataComplete(e)
 {
     FBTest.progress("Request call back called");
 
-    var panel = FBTest.getPanel("console");
-    var row = panel.panelNode.querySelector(".logRow.logRow-spy.loaded");
-
-    // Expand XHR entry within the Console panel. The browser must not freeze
-    // and the response body must be properly displayed.
-    FBTest.expandElements(row, "spyTitleCol", "spyCol");
-
-    // Get response body element and check its content. Note that the displayed text
-    // is limited in case of large responses.
-    var limit = FBTest.getPref("netDisplayedResponseLimit");
-    var responseBody = FW.FBL.getElementsByClass(row,
-        "netInfoResponseText", "netInfoText");
-
-    FBTest.ok(responseBody, "Response body must be presented");
-
-    // Generate response text (the same as the PHP file).
-    var responseText = "";
-    for (var i=0; i<80000; i++)
-        responseText += i + " ";
-
-    FBTest.compare(468890, responseText.length, "Response must have correct size");
-
     var config = {
         tagName: "div",
-        classes: "netInfoResponseSizeLimit"
+        classes: "logRow logRow-spy loaded"
     };
 
-    // It takes some time to display huge response so, wait for the last message
-    // saying: a limit has been reached...
-    FBTest.waitForDisplayedElement("console", config, function()
+    FBTest.waitForDisplayedElement("console", config, function(row)
     {
-        // Compare expected and actuall (displayed) response text.
-        var text1 = responseText.substr(0, limit);
-        var text2 = responseBody[0].textContent.substr(0, limit);
-        FBTest.compare(text1, text2, "The response text must be properly displayed");
-        FBTest.testDone("issue1693.DONE");
+        var panel = FBTest.getPanel("console");
+    
+        var config = {
+            tagName: "div",
+            classes: "netInfoResponseSizeLimit"
+        };
+    
+        // It takes some time to display huge response so, wait for the last message
+        // saying: a limit has been reached...
+        FBTest.waitForDisplayedElement("console", config, function(responseSizeLimit)
+        {
+            // Get response body element and check its content. Note that the displayed text
+            // is limited in case of large responses.
+            var limit = FBTest.getPref("netDisplayedResponseLimit");
+            var responseBody = FW.FBL.getAncestorByClass(responseSizeLimit, "netInfoResponseText netInfoText");
+
+            // xxxsz: For some reason this is called twice, and one time the response body is undefined
+            if (responseBody)
+            {
+                // Generate response text (the same as the PHP file).
+                var responseText = "";
+                for (var i = 0; i < 80000; i++)
+                    responseText += i + " ";
+            
+                // Compare expected and actuall (displayed) response text.
+                var text1 = responseText.substr(0, limit);
+                var text2 = responseBody.textContent.substr(0, limit);
+                FBTest.compare(text1, text2, "The response text must be properly displayed");
+                FBTest.testDone("issue1693.DONE");
+            }
+        });
+
+        // Expand XHR entry within the Console panel. The browser must not freeze
+        // and the response body must be properly displayed.
+        FBTest.expandElements(row, "spyTitleCol", "spyCol");
     });
 }
