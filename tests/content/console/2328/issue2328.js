812f13d9242d3052fe54d74f53d85b3bb9f60f16
Sebastian Zartner <sebastianzartner@gmail.com>
2014-02-21 09:41:36 +0100

    Fixed FBTest console/2328/issue2328.js

--- tests/content/console/2328/issue2328.js
+++ tests/content/console/2328/issue2328.js
@@ -11,32 +11,31 @@ function runTest()
     var prefOrigValue = FBTest.getPref("showXMLHttpRequests");
     FBTest.setPref("showXMLHttpRequests", true);
 
-    FBTest.enableConsolePanel();
     FBTest.openNewTab(basePath + "console/2328/issue2328.html", function(win)
     {
         FBTest.sysout("issue2328; Test page loaded.");
 
         FBTest.openFirebug();
-        FBTest.selectPanel("console");
+        FBTest.enableConsolePanel(function() {
+            // Create listener for mutation events.
+            var doc = FBTest.getPanelDocument();
+            var recognizer = new MutationRecognizer(doc.defaultView, "div",
+                {"class": "logRow logRow-spy loaded"});
 
-        // Create listener for mutation events.
-        var doc = FBTest.getPanelDocument();
-        var recognizer = new MutationRecognizer(doc.defaultView, "div",
-            {"class": "logRow logRow-spy loaded"});
+            // Wait for an error log in the Console panel.
+            recognizer.onRecognize(function (element)
+            {
+                // Verify error log in the console.
+                var expectedResult = "GET " + basePath + "console/2328/issue2328.php";
+                var spyFullTitle = FW.FBL.getElementByClass(element, "spyFullTitle");
+                FBTest.compare(expectedResult, spyFullTitle.textContent, "There must be a XHR log");
 
-        // Wait for an error log in the Console panel.
-        recognizer.onRecognize(function (element)
-        {
-            // Verify error log in the console.
-            var expectedResult = "GET " + basePath + "console/2328/issue2328.php";
-            var spyFullTitle = FW.FBL.getElementByClass(element, "spyFullTitle");
-            FBTest.compare(expectedResult, spyFullTitle.textContent, "There must be a XHR log");
+                FBTest.setPref("showXMLHttpRequests", prefOrigValue);
+                FBTest.testDone("issue2328; DONE");
+            });
 
-            FBTest.setPref("showXMLHttpRequests", prefOrigValue);
-            FBTest.testDone("issue2328; DONE");
+            // Run test implemented on the page.
+            FBTest.click(win.document.getElementById("testButton"));
         });
-
-        // Run test implemented on the page.
-        FBTest.click(win.document.getElementById("testButton"));
     });
 }
