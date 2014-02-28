4e5c87136c41b43df3c6e98a377146354c2f2ca5
Sebastian Zartner <sebastianzartner@gmail.com>
2014-02-28 13:58:40 +0100

    Fixed FBTest console/5033/issue5033.js

--- tests/content/console/5033/issue5033.js
+++ tests/content/console/5033/issue5033.js
@@ -3,22 +3,29 @@ function runTest()
     FBTest.openNewTab(basePath + "console/5033/issue5033.html", function(win)
     {
         FBTest.openFirebug(function() {
-            // xxxFlorent: Remove this when Firebug 2.0 is out.
-            // Don't display the "Slow JSD" warning message.
-            FBTest.setPref("showSlowJSDMessage", false);
-
-            FBTest.enablePanels(["script", "console"], function() {
+            FBTest.enablePanels(["script", "console"], function()
+            {
                 FBTest.waitForBreakInDebugger(null, 10, true, function()
                 {
                     FBTest.waitForDebuggerResume(function ()
                     {
-                        var panel = FBTest.selectPanel("console");
-                        var result = panel.panelNode.querySelector(".logRow:not(.logRow-command)");
+                        var panelNode = FBTest.selectPanel("console").panelNode;
 
-                        if (FBTest.ok(result, "Result must exist"))
-                            FBTest.compare(20, result.textContent, "Result must be correct");
+                        var config = {
+                            tagName: "div",
+                            classes: "logRow",
+                            onlyMutations: true
+                        };
 
-                        FBTest.testDone("issue5033.DONE");
+                        FBTest.waitForDisplayedElement("console", config, function(row)
+                        {
+                            var result = panelNode.querySelector(".logRow:not(.logRow-command)");
+
+                            if (FBTest.ok(result, "Result must exist"))
+                                FBTest.compare(20, result.textContent, "Result must be correct");
+
+                            FBTest.testDone("issue5033.DONE");
+                        });
                     });
 
                     // Click step over five times to resume the debugger.
@@ -26,12 +33,8 @@ function runTest()
                     {
                         FBTest.waitForBreakInDebugger(null, 12, false, function()
                         {
-                            FBTest.waitForBreakInDebugger(null, 2, false, function()
+                            FBTest.waitForBreakInDebugger(null, 12, false, function()
                             {
-                                FBTest.waitForBreakInDebugger(null, 3, false, function()
-                                {
-                                    FBTest.clickStepOverButton();
-                                });
                                 FBTest.clickStepOverButton();
                             });
                             FBTest.clickStepOverButton();
