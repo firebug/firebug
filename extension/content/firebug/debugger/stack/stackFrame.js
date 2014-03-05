8353e8d14d7529bb2a53025369aaae7e5279e682
Jan Odvarko <odvarko@gmail.com>
2014-03-05 14:31:16 +0100

    Improve regular expression (Issue 7230)

--- extension/content/firebug/debugger/stack/stackFrame.js
+++ extension/content/firebug/debugger/stack/stackFrame.js
@@ -302,23 +302,18 @@ StackFrame.resumeShowStackTrace = function(){}
 
 // ********************************************************************************************* //
 
-// functionName@fileName:lineNo
-var reErrorStackLine = /^(.*)@(.*):(\d*)$/;
-
 // Firefox 30 introduced also column number in the URL (see Bug 762556)
 // functionName@fileName:lineNo:columnNo
 // xxxHonza: at some point we might want to utilize the column number as well.
-var reErrorStackLine2 = /^(.*)@(.*):(\d*):(\d*)$/;
+// The regular expression can be simplified to expect both (:line:column) as soon
+// as Firefox 30 (Fx30) is the minimum required version.
+var reErrorStackLine = /^(.*)@(.*?):(\d*)(?::(\d*))?$/
 
 StackFrame.parseToStackFrame = function(line, context)
 {
-    var m = reErrorStackLine2.exec(line);
+    var m = reErrorStackLine.exec(line);
     if (!m)
-    {
-        var m = reErrorStackLine.exec(line);
-        if (!m)
-            return;
-    }
+        return;
 
     return new StackFrame({href:m[2]}, m[3], m[1], [], null, null, context);
 };
