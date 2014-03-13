79e0985a0ba8d116648c7d3d9bac6eb6f110914e
Jan Odvarko <odvarko@gmail.com>
2014-03-13 15:27:32 +0100

    FBTest: fixing assert, error, exception

--- tests/content/console/api/exception.js
+++ tests/content/console/api/exception.js
@@ -2,20 +2,26 @@ function runTest()
 {
     FBTest.openNewTab(basePath + "console/api/exception.html", function(win)
     {
-        FBTest.openFirebug(function() {
-            FBTest.enableConsolePanel(function(win)
+        FBTest.enablePanels(["console", "script"], function(win)
+        {
+            var text = "asdf.asdf = 1;";
+            FBTest.waitForDisplayedText("console", text, function()
             {
-                var config = {tagName: "div", classes: "logRow logRow-errorMessage"};
-                FBTest.waitForDisplayedElement("console", config, function(row)
-                {
-                    var reTextContent = new RegExp("ReferenceError: asdf is not defined\\s*asdf.asdf = 1;\\s*" +
-                        FW.FBL.$STRF("Line", ["exception.html", 35]).replace(/([\\"'\(\)])/g, "\\$1"));
-                    FBTest.compare(reTextContent, row.textContent, "The proper message must be displayed.");
-                    FBTest.testDone("console.exception.DONE");
-                });
+                var panel = FBTest.getSelectedPanel();
+                var row = panel.panelNode.getElementsByClassName("logRow-errorMessage")[0];
 
-                FBTest.click(win.document.getElementById("testButton"));
+                var reTextContent = new RegExp(
+                    "ReferenceError: asdf is not defined\\s*asdf.asdf = 1;\\s*" +
+                    FW.FBL.$STRF("Line", ["exception.html", 35]).
+                    replace(/([\\"'\(\)])/g, "\\$1"));
+
+                FBTest.compare(reTextContent, row.textContent,
+                    "The proper message must be displayed.");
+
+                FBTest.testDone();
             });
+
+            FBTest.clickContentButton(win, "testButton");
         });
     });
 }
