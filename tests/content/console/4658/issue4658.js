07eddf2a4bfe4310be0f39b3c778e80fbde56d20
Jan Odvarko <odvarko@gmail.com>
2014-03-14 13:26:32 +0100

    FBTest: fix for console/4658

--- tests/content/console/4658/issue4658.js
+++ tests/content/console/4658/issue4658.js
@@ -3,21 +3,21 @@ function runTest()
     FBTest.setPref("preferJSDSourceLinks", true);
     FBTest.openNewTab(basePath + "console/4658/issue4658.html", function(win)
     {
-        FBTest.openFirebug(function() {
-            FBTest.enableScriptPanel();
-            FBTest.enableConsolePanel(function(win)
+        FBTest.enablePanels(["console", "script"], function(win)
+        {
+            var config = {tagName: "div", classes: "logRow logRow-debug"};
+            FBTest.waitForDisplayedElement("console", config, function(row)
             {
-                var config = {tagName: "div", classes: "logRow logRow-debug"};
-                FBTest.waitForDisplayedElement("console", config, function(row)
-                {
-                    var expected = new RegExp("FirebugBug\\s*" + FW.FBL.$STRF("Line",
-                        ["clickedFirebugBug.js", 6]).replace(/([\\"'\(\)])/g, "\\$1"));
-                    FBTest.compare(expected, row.textContent, "The proper message must be displayed.");
-                    FBTest.testDone("issue4658.DONE");
-                });
+                var expected = new RegExp("FirebugBug\\s*" + FW.FBL.$STRF("Line",
+                    ["clickedFirebugBug.js", 6]).replace(/([\\"'\(\)])/g, "\\$1"));
 
-                FBTest.click(win.document.getElementById("testButton"));
+                FBTest.compare(expected, row.textContent,
+                    "The proper message must be displayed.");
+
+                FBTest.testDone(;
             });
+
+            FBTest.clickContentButton(win, "testButton");
         });
     });
 }
