f7e5db1bf8b2145a3cd3cb3560ab41deb3902d13
Jan Odvarko <odvarko@gmail.com>
2014-03-14 17:19:45 +0100

    FBTest: fix commandLine/1854

--- tests/content/commandLine/1854/issue1854.js
+++ tests/content/commandLine/1854/issue1854.js
@@ -1,14 +1,18 @@
 function runTest()
 {
-    FBTest.openNewTab(basePath + "commandLine/1854/issue1854.html", function(win)
+    var url = basePath + "commandLine/1854/issue1854.html";
+    FBTest.openNewTab(url, function()
     {
         // Step 1: Open Firebug
-        FBTest.openFirebug(function() {
+        FBTest.openFirebug(function()
+        {
             // Step 2: Enable the Script and the Console panel
             // Step 3: Switch to the Console panel
-            FBTest.enablePanels(["console", "script"], function() {
+            FBTest.enablePanels(["console", "script"], function()
+            {
                 // Step 4: Reload the page
-                FBTest.reload(function() {
+                FBTest.reload(function(win)
+                {
                     // Asynchronously wait for result in the Console panel.
                     var config = {tagName: "div", classes: "logRow logRow-command"};
                     FBTest.waitForDisplayedElement("console", config, function(row)
@@ -16,15 +20,22 @@ function runTest()
                         FBTest.compare(">>> debug(showOutput)", row.textContent,
                             "The command line should display: >>> debug(showOutput)");
 
-                        // Asynchronously wait for break in debugger.
-                        FBTest.waitForBreakInDebugger(FW.Firebug.chrome, 11, false, function(row)
+                        FBTest.waitForBreakpoint(url, 11, function()
                         {
-                            FBTest.clickContinueButton();
-                            FBTest.testDone("issue1854.DONE");
-                        });
+                            var config = {tagName: "div", classes: "breakpoint"};
+                            FBTest.waitForDisplayedElement("script", config, function(element)
+                            {
+                                // Asynchronously wait for break in debugger.
+                                FBTest.waitForBreakInDebugger(null, 11, false, function(row)
+                                {
+                                    FBTest.clickContinueButton();
+                                    FBTest.testDone();
+                                });
 
-                        // Step 6: Click the 'Show output' button
-                        FBTest.click(win.document.getElementById("showOutput"));
+                                // Step 6: Click the 'Show output' button
+                                FBTest.clickContentButton(win, "showOutput");
+                            });
+                        });
                     });
 
                     // Step 5: Type debug(showOutput) into the Command Line and hit Enter
