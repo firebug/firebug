0428fa4b02eff2cda59cba082edb2ed06d1236af
Jan Odvarko <odvarko@gmail.com>
2014-03-14 16:34:58 +0100

    FBTest: fix script/5044

--- tests/content/script/5044/issue5044.js
+++ tests/content/script/5044/issue5044.js
@@ -3,7 +3,7 @@ function runTest()
     FBTest.openNewTab(basePath + "script/5044/issue5044.html", function(win)
     {
         // 1. Open Firebug
-        FBTest.openFirebug(function ()
+        FBTest.openFirebug(function()
         {
             // 2. Enable and switch to the Script panel
             FBTest.enableScriptPanel(function(win)
@@ -11,14 +11,14 @@ function runTest()
                 // 3. Press Ctrl/⌘+Alt+B
                 FBTest.sendShortcut("b", {accelKey: true, altKey: true});
 
-                FBTest.waitForBreakInDebugger(FW.Firebug.chrome, 10, false, function(row)
+                FBTest.waitForBreakInDebugger(FW.Firebug.chrome, 1, false, function(row)
                 {
                     FBTest.clickContinueButton();
                     FBTest.testDone();
                 });
 
                 // 4. Click the Say hello button above
-                FBTest.click(win.document.getElementById("sayHello"));
+                FBTest.clickContentButton(win, "sayHello");
             });
         });
     });
