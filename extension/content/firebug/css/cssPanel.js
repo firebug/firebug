609eed720762ff8eaa5b545d32f842001157f072
Jakob Kaltenbrunner <jakob.kaltenbrunner@gmx.at>
2014-03-16 20:00:14 +0100

    Issue 7213: Split firebug/css/cssPanel into more modules - moved StyleSheetEditor object from cssPanel.js to styleSheetEditor.js

--- extension/content/firebug/css/cssPanel.js
+++ extension/content/firebug/css/cssPanel.js
@@ -29,17 +29,17 @@ define([
     "firebug/css/cssPanelUpdater",
     "firebug/css/cssReps",
     "firebug/css/cssRuleEditor",
+    "firebug/css/styleSheetEditor",
     "firebug/editor/baseEditor",
     "firebug/editor/editor",
     "firebug/editor/inlineEditor",
-    "firebug/editor/sourceEditor",
     "firebug/debugger/script/sourceLink",
     "firebug/css/cssPanelMutationObserver",
 ],
 function(Firebug, FBTrace, Arr, Css, Dom, Domplate, Events, Locale, Obj, Options, Persist, Search,
     Str, System, Url, Wrapper, Xml, Menu, Panel, FirebugReps, SearchBox, Win, CSSAutoCompleter,
-    CSSModule, CSSPanelUpdater, CSSReps, CSSRuleEditor, BaseEditor, Editor, InlineEditor,
-    SourceEditor, SourceLink) {
+    CSSModule, CSSPanelUpdater, CSSReps, CSSRuleEditor, StyleSheetEditor, BaseEditor, Editor,
+    InlineEditor, SourceLink) {
 
 // ********************************************************************************************* //
 // Constants
@@ -2417,120 +2417,6 @@ CSSEditor.prototype = domplate(InlineEditor.prototype,
         return {value: "#" + expr, selection: [offset+1, offsetEnd+1]};
     }
 });
-
-// ********************************************************************************************* //
-// StyleSheetEditor
-
-/**
- * StyleSheetEditor represents the full-sized editor used for Source/Live Edit
- * within the CSS panel.
- */
-function StyleSheetEditor(doc)
-{
-    this.box = this.tag.replace({}, doc, this);
-
-    this.onEditorTextChangeListener = this.onEditorTextChange.bind(this);
-    var config = {
-        mode: "css",
-        readOnly: false,
-        gutters: []
-    };
-    // Initialize source editor, then append to the box.
-    this.editor = new SourceEditor();
-    this.editor.init(this.box, config, this.onEditorInitialize.bind(this));
-}
-
-StyleSheetEditor.prototype = domplate(BaseEditor,
-{
-    multiLine: true,
-
-    tag: DIV({"class": "styleSheetEditor fullPanelEditor"}),
-
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
-    onEditorInitialize: function()
-    {
-        this.editor.addEventListener(SourceEditor.Events.textChange,
-            this.onEditorTextChangeListener);
-    },
-
-    getValue: function()
-    {
-        return this.editor.getText();
-    },
-
-    setValue: function(value)
-    {
-        return this.editor.setText(value, "css");
-    },
-
-    show: function(target, panel, value, textSize)
-    {
-        this.target = target;
-        this.panel = panel;
-
-        // Show the box that editor already is appended to.
-        this.panel.panelNode.appendChild(this.box);
-        this.editor.setText(value, "css");
-
-        // match CSSModule.getEditorOptionKey
-        var command = Firebug.chrome.$("cmd_firebug_togglecssEditMode");
-        command.setAttribute("checked", true);
-    },
-
-    hide: function()
-    {
-        var command = Firebug.chrome.$("cmd_firebug_togglecssEditMode");
-        command.setAttribute("checked", false);
-
-        if (this.box.parentNode == this.panel.panelNode)
-            this.panel.panelNode.removeChild(this.box);
-
-        delete this.target;
-        delete this.panel;
-        delete this.styleSheet;
-    },
-
-    saveEdit: function(target, value, previousValue)
-    {
-        if (FBTrace.DBG_CSS)
-            FBTrace.sysout("StyleSheetEditor.saveEdit", arguments);
-
-        CSSModule.freeEdit(this.styleSheet, value);
-    },
-
-    beginEditing: function()
-    {
-        if (FBTrace.DBG_CSS)
-            FBTrace.sysout("StyleSheetEditor.beginEditing", arguments);
-
-        this.editing = true;
-    },
-
-    endEditing: function()
-    {
-        if (FBTrace.DBG_CSS)
-            FBTrace.sysout("StyleSheetEditor.endEditing", arguments);
-
-        this.editing = false;
-        this.panel.refresh();
-        return true;
-    },
-
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
-    onEditorTextChange: function()
-    {
-        Editor.update();
-    },
-
-    scrollToLine: function(line, offset)
-    {
-        this.editor.scrollToLine(line);
-    }
-});
-
-Firebug.StyleSheetEditor = StyleSheetEditor;
 
 // ********************************************************************************************* //
 
---
+++ extension/content/firebug/css/styleSheetEditor.js
@@ -0,0 +1,137 @@
+/* See license.txt for terms of usage */
+
+define([
+    "firebug/firebug",
+    "firebug/lib/trace",
+    "firebug/lib/domplate",
+    "firebug/css/cssModule",
+    "firebug/editor/baseEditor",
+    "firebug/editor/editor",
+    "firebug/editor/sourceEditor",
+],
+function(Firebug, FBTrace, Domplate, CSSModule, BaseEditor, Editor, SourceEditor) {
+
+// ********************************************************************************************* //
+// Constants
+
+var {DIV} = Domplate;
+
+// ********************************************************************************************* //
+// StyleSheetEditor
+
+/**
+ * StyleSheetEditor represents the full-sized editor used for Source/Live Edit
+ * within the CSS panel.
+ */
+function StyleSheetEditor(doc)
+{
+    this.box = this.tag.replace({}, doc, this);
+
+    this.onEditorTextChangeListener = this.onEditorTextChange.bind(this);
+    var config = {
+        mode: "css",
+        readOnly: false,
+        gutters: []
+    };
+    // Initialize source editor, then append to the box.
+    this.editor = new SourceEditor();
+    this.editor.init(this.box, config, this.onEditorInitialize.bind(this));
+}
+
+StyleSheetEditor.prototype = domplate(BaseEditor,
+{
+    multiLine: true,
+
+    tag: DIV({"class": "styleSheetEditor fullPanelEditor"}),
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    onEditorInitialize: function()
+    {
+        this.editor.addEventListener(SourceEditor.Events.textChange,
+            this.onEditorTextChangeListener);
+    },
+
+    getValue: function()
+    {
+        return this.editor.getText();
+    },
+
+    setValue: function(value)
+    {
+        return this.editor.setText(value, "css");
+    },
+
+    show: function(target, panel, value, textSize)
+    {
+        this.target = target;
+        this.panel = panel;
+
+        // Show the box that editor already is appended to.
+        this.panel.panelNode.appendChild(this.box);
+        this.editor.setText(value, "css");
+
+        // match CSSModule.getEditorOptionKey
+        var command = Firebug.chrome.$("cmd_firebug_togglecssEditMode");
+        command.setAttribute("checked", true);
+    },
+
+    hide: function()
+    {
+        var command = Firebug.chrome.$("cmd_firebug_togglecssEditMode");
+        command.setAttribute("checked", false);
+
+        if (this.box.parentNode == this.panel.panelNode)
+            this.panel.panelNode.removeChild(this.box);
+
+        delete this.target;
+        delete this.panel;
+        delete this.styleSheet;
+    },
+
+    saveEdit: function(target, value, previousValue)
+    {
+        if (FBTrace.DBG_CSS)
+            FBTrace.sysout("StyleSheetEditor.saveEdit", arguments);
+
+        CSSModule.freeEdit(this.styleSheet, value);
+    },
+
+    beginEditing: function()
+    {
+        if (FBTrace.DBG_CSS)
+            FBTrace.sysout("StyleSheetEditor.beginEditing", arguments);
+
+        this.editing = true;
+    },
+
+    endEditing: function()
+    {
+        if (FBTrace.DBG_CSS)
+            FBTrace.sysout("StyleSheetEditor.endEditing", arguments);
+
+        this.editing = false;
+        this.panel.refresh();
+        return true;
+    },
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    onEditorTextChange: function()
+    {
+        Editor.update();
+    },
+
+    scrollToLine: function(line, offset)
+    {
+        this.editor.scrollToLine(line);
+    }
+});
+
+// ********************************************************************************************* //
+// Registration
+
+return StyleSheetEditor;
+
+// ********************************************************************************************* //
+});
