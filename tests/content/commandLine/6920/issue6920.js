770244bf5cddf63de2384c619ef382ceb940c4ee
Jan Odvarko <odvarko@gmail.com>
2014-03-31 18:31:33 +0200

    FBTest: new test commandLine/6920

---
+++ tests/content/commandLine/6920/issue6920.js
@@ -0,0 +1,19 @@
+function runTest()
+{
+    FBTest.openNewTab(basePath + "commandLine/6920/issue6920.html", function(win)
+    {
+        FBTest.enablePanels(["console"], function()
+        {
+            var CommandEditor = FW.Firebug.CommandEditor;
+            CommandEditor.setText("function test() { return true; }", true);
+
+            var context = FW.Firebug.currentContext;
+            CommandEditor.prettyPrint(context).then((code) =>
+            {
+                var expected = "function test() {\n    return true;\n}\n";
+                FBTest.compare(expected, code, "The code must be formatted");
+                FBTest.testDone();
+            });
+        });
+    });
+}
