c22d014d57947655be16cc9961aa1e7e440522a6
Farshid Beheshti <farshid.beheshti@gmail.com>
2014-04-02 21:43:09 +0430

    Make the aFunction global to avoid breaking by the bug platform #853847.

--- tests/content/script/breakpoints/6253/issue6253.js
+++ tests/content/script/breakpoints/6253/issue6253.js
@@ -4,27 +4,43 @@ function runTest()
     {
         FBTest.enableScriptPanel(function(win)
         {
-            FBTest.setBreakpoint(null, "issue6253.html", 10, null, function(row)
+            FBTest.setBreakpoint(null, "issue6253.html", 9, null, function(row)
             {
-                // Little wait to the breakpoint finds the right sit(line 12).
-                function delay(func)
+                // In first try, It's likely pass the test if there is no problem,
+                // but test the chance a second time if test fails to make sure
+                // the reason causes failing is something else than a haste.
+                var Number_Of_Attempts = 2;
+
+                tryTest(Number_Of_Attempts);
+
+                function tryTest(numberOfAttempts)
                 {
-                    setTimeout(func());
+                    setTimeout(function ()
+                    {
+                        var breakpointLeftLineNine = !FBTest.hasBreakpoint(9);
+
+                        // Interested in trying again, if the breakpoint hasn't
+                        // left line #9 yet?
+                        if (!breakpointLeftLineNine && --numberOfAttempts)
+                        {
+                            // Try again with a delay of 30 millisecond.
+                            setTimeout(arguments.callee, 30);
+                            FBTest.progress("Waiting yet for the breakpoint to finds " +
+                                "the right sit .....");
+                            return;
+                        }
+
+                        FBTest.ok(breakpointLeftLineNine,
+                            "Then, The bp must have left line 9");
+
+                        var breakpointHasMoved = FBTest.hasBreakpoint(11);
+
+                        FBTest.ok(breakpointHasMoved,
+                            "Then, The bp must have moved to line 11");
+
+                        FBTest.testDone();
+                    }, 0);
                 }
-
-                delay(function ()
-                {
-                    var breakpointLeftLineTen = !FBTest.hasBreakpoint(10);
-                    FBTest.ok(breakpointLeftLineTen,
-                        "At this time, There must not be a bp at line 10");
-
-                    var breakpointHasMoved = breakpointLeftLineTen &&
-                        FBTest.hasBreakpoint(12);
-                    FBTest.ok(breakpointHasMoved,
-                        "Then, The bp must have moved to line 12");
-
-                    FBTest.testDone();
-                });
             });
         });
     });
