acc92de0bf4f1f8bd15e31ea8ad9589a09897436
Sebastian Zartner <sebastianzartner@gmail.com>
2014-04-08 23:44:56 +0200

    Added steps to FBTest

--- tests/content/console/5216/issue5216.js
+++ tests/content/console/5216/issue5216.js
@@ -2,8 +2,10 @@ function runTest()
 {
     FBTest.openNewTab(basePath + "console/5216/issue5216.html", (win) =>
     {
+        // 1. Open Firebug
         FBTest.openFirebug(() =>
         {
+            // 2. Enable and switch to the Console panel
             FBTest.enableConsolePanel(() =>
             {
                 var config = {
@@ -13,6 +15,7 @@ function runTest()
                 FBTest.waitForDisplayedElement("console", config, (errorBreakpoint) =>
                 {
                     var row = FW.FBL.getAncestorByClass(errorBreakpoint, "logRow");
+                    // 4. Right-click the error
                     FBTest.checkIfContextMenuCommandExists(row, "breakOnThisError", (exists) =>
                     {
                         FBTest.ok(exists,
@@ -21,6 +24,7 @@ function runTest()
                     }, {x: row.offsetWidth / 2, y: row.offsetHeight / 2});
                 });
 
+                // 3. Click the 'Cause exception' button
                 FBTest.clickContentButton(win, "causeException");
             });
         });
