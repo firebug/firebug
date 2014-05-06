16c3c72fb30375a45ae0de314a2c68188767cecd
Jan Odvarko <odvarko@gmail.com>
2014-05-06 13:33:09 +0200

    FBTest: fix for console/4322

--- tests/content/console/4322/issue4322.js
+++ tests/content/console/4322/issue4322.js
@@ -2,7 +2,8 @@ function runTest()
 {
     FBTest.openNewTab(basePath + "console/4322/issue4322.html", function(win)
     {
-        FBTest.openFirebug(function () {
+        FBTest.openFirebug(function ()
+        {
             FBTest.enableConsolePanel(function(win)
             {
                 var config = {tagName: "div", classes: "logRow logRow-errorMessage"};
@@ -12,9 +13,14 @@ function runTest()
                     FBTest.compare(/getSession is not defined/, title.textContent,
                         "The error title must match.");
 
+                    var expected = FW.FBL.$STRF("Line", ["issue4322.html", 10]);
+
+                    // Case for Firefox 31+ [Fx31]
+                    if (FBTest.compareFirefoxVersion("30") > 0)
+                        expected = FW.FBL.$STRF("LineAndCol", ["issue4322.html", 10, 16]);
+
                     var link = row.querySelector(".objectLink.objectLink-sourceLink");
-                    FBTest.compare(FW.FBL.$STRF("Line", ["issue4322.html", 10]), link.textContent,
-                        "The source link must match.");
+                    FBTest.compare(expected, link.textContent, "The source link must match.");
 
                     FBTest.testDone();
                 });
