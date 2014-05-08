86f461babda96a14b64f38da6db4546bfd10b1d9
Sebastian Zartner <sebastianzartner@gmail.com>
2014-05-08 08:27:46 +0200

    Issue 7450 (Use lowercase hexadecimal for colours in the CSS window) https://code.google.com/p/fbug/issues/detail?id=7450

--- extension/content/firebug/lib/css.js
+++ extension/content/firebug/lib/css.js
@@ -972,7 +972,7 @@ Css.rgbToHex = function(value)
     function convertRGBToHex(r, g, b)
     {
         return "#" + ((1 << 24) + (r << 16) + (g << 8) + (b << 0)).
-            toString(16).substr(-6).toUpperCase();
+            toString(16).substr(-6);
     }
 
     value = Css.colorNameToRGB(value);
