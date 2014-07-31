3c0ed0c8c049d3da39090d748657e140ab47f798
Sebastian Zartner <sebastianzartner@gmail.com>
2014-07-31 13:27:02 +0200

    Removed incorrect change of Continue button tooltip

--- extension/content/firebug/cookies/cookiePanel.js
+++ extension/content/firebug/cookies/cookiePanel.js
@@ -265,13 +265,6 @@ CookiePanel.prototype = Obj.extend(ActivablePanel,
         CookiePermissions.updatePermButton(this.context);
 
         this.showToolbarButtons("fbCookieButtons", true);
-
-        if (Firebug.chrome.setGlobalAttribute)
-        {
-            Firebug.chrome.setGlobalAttribute("cmd_firebug_resumeExecution", "breakable", "true");
-            Firebug.chrome.setGlobalAttribute("cmd_firebug_resumeExecution", "tooltiptext",
-                Locale.$STR("cookies.Break On Cookie"));
-        }
     },
 
     hide: function()
