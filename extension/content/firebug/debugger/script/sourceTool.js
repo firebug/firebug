firebug/firebug

commit c089d2f365efc40aa4778c8c6fa9bb64104590aa

Don't include sourceMappingURLs in the parsing for sourceURLs as they are unrelated

extension/content/firebug/debugger/script/sourceTool.js
@@ -752,7 +752,7 @@ function computeDynamicUrl(script, context)
 
     // 'displayURL' is not correctly recognized in all cases, so fix the remaining cases here.
     // (see issue 6813, issue 6969, https://bugzil.la/981987 and https://bugzil.la/1065259)
-    var reSourceURL = /\/\/[@#]\ssource(Mapping)?URL=\s*(\S*?)\s*$/m;
+    var reSourceURL = /\/\/[@#]\ssourceURL=\s*(\S*?)\s*$/m;
     var sourceURLMatch = script.source.text.match(reSourceURL);
     if (sourceURLMatch)
     {

commit 2d98125bde8b3ab6190a23f64e738c0bb0e340ab

Fixed sourceURL generation caused by https://github.com/firebug/firebug/commit/c089d2f365efc40aa4778c8c6fa9bb64104590aa

extension/content/firebug/debugger/script/sourceTool.js
@@ -757,14 +757,16 @@ function computeDynamicUrl(script, context)
     // (see issue 6813, issue 6969, https://bugzil.la/981987 and https://bugzil.la/1065259)
     var reSourceURL = /\/\/[@#]\ssourceURL=\s*(\S*?)\s*$/m;
     var sourceURLMatch = script.source.text.match(reSourceURL);
+    FBTrace.sysout("sourceURLMatch", sourceURLMatch);
     if (sourceURLMatch)
     {
-        var url = sourceURLMatch[2];
+        url = sourceURLMatch[1];
 
         // Add context info to the sourceURL so eval'd sources are grouped
         // correctly in the source file list
         if (url && !url.contains("://"))
         {
+            FBTrace.sysout("introductionScript", script.source.introductionScript.url);
             var loc = script.source.introductionScript ?
                 script.source.introductionScript.url : script.source.url;
 
@@ -783,6 +785,7 @@ function computeDynamicUrl(script, context)
             }
         }
 
+        FBTrace.sysout("url", {script: script, loc: loc, url: Url.normalizeURL(url)});
         return Url.normalizeURL(url);
     }
 